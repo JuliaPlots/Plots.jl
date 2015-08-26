fff099ff84495bf6ccc3d709f1812197e8d4f41d
Thomas Breloff <tom@breloff.com>
2015-08-25 22:49:48 -0400

    working on doc/interface

--- src/Plot.jl
+++ src/Plot.jl
@@ -137,20 +137,25 @@ doc"""
 The main plot command.  You must call `plotter(:ModuleName)` to set the current plotting environment first.
 Commands are converted into the relevant plotting commands for that package:
 
+```
   plotter(:Gadfly)
   plot(1:10)    # this calls `y = 1:10; Gadfly.plot(x=1:length(y), y=y)`
   plotter(:Qwt)
   plot(1:10)    # this calls `Qwt.plot(1:10)`
+```
 
 Use `plot` to create a new plot object, and `plot!` to add to an existing one:
 
+```
   plot(args...; kw...)                  # creates a new plot window, and sets it to be the currentPlot
   plot!(args...; kw...)                 # adds to the `currentPlot`
   plot!(plotobj, args...; kw...)        # adds to the plot `plotobj`
+```
 
 Now that you know which plot object you're updating (new, current, or other), I'll leave it off for simplicity.
 Here are some various args to supply, and the implicit mapping (AVec == AbstractVector and AMat == AbstractMatrix):
 
+```
   plot(y::AVec; kw...)                                # one line... x = 1:length(y)
   plot(x::AVec, y::AVec; kw...)                       # one line (will assert length(x) == length(y))
   plot(y::AMat; kw...)                                # multiple lines (one per column of x), all sharing x = 1:size(y,1)
@@ -162,19 +167,23 @@ Here are some various args to supply, and the implicit mapping (AVec == Abstract
   plot(x::AVec, y::AVec{AVec}; kw...)                 # multiple lines, will assert length(x) == length(y[i])
   plot(x::AVec{AVec}, y::AVec{AVec}; kw...)           # multiple lines, will assert length(x[i]) == length(y[i])
   plot(n::Integer; kw...)                             # n lines, all empty (for updating plots)
+```
 
   TODO: DataFrames
 
 You can swap out `plot` for `subplot`.  Each line will go into a separate plot.  Use the layout keyword:
 
+```
   y = rand(100,3)
   subplot(y; layout=(2,2), kw...)           # creates 3 lines going into 3 separate plots, laid out on a 2x2 grid (last row is filled with plot #3)
   subplot(y; layout=(1,3), kw...)           # again 3 plots, all in the same row
   subplot(y; layout=[1,[2,3]])              # pass a nested Array to fully specify the layout.  here the first plot will take up the first row, 
                                             # and the others will share the second row
+```
 
 Some keyword arguments you can set:
 
+```
   axis            # :left or :right
   color           # can be a string ("red") or a symbol (:red) or a ColorsTypes.jl Colorant (RGB(1,0,0)) or :auto (which lets the package pick)
   label           # string or symbol, applies to that line, may go in a legend
@@ -194,9 +203,11 @@ Some keyword arguments you can set:
   windowtitle     # string or symbol, set the title of the enclosing windowtitle
   screen          # Integer, move enclosing window to this screen number (for multiscreen desktops)
   show            # true or false, show the plot (in case you don't want the window to pop up right away)
+```
 
 If you don't include a keyword argument, these are the defaults:
   
+```
   axis = :left
   color = :auto
   label = automatically generated (y1, y2, ...., or y1 (R), y2 (R) for the right axis)
@@ -216,11 +227,14 @@ If you don't include a keyword argument, these are the defaults:
   windowtitle = ""
   screen = 1
   show = true
+```
 
 When plotting multiple lines, you can give every line the same trait by using the singular, or add an "s" to pluralize.
   (yes I know it's not gramatically correct, but it's easy to use and implement)
 
+```
   plot(rand(100,2); colors = [:red, RGB(.5,.5,0)], axiss = [:left, :right], width = 5)  # note the width=5 is applied to both lines
+```
 
 """
 
