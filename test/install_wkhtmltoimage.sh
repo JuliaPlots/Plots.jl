67ec31c75a66eb804180084203027b60fe0bf59f
Thomas Breloff <tom@breloff.com>
2016-04-29 13:52:22 -0400

    travis

--- test/install_wkhtmltoimage.sh
+++ test/install_wkhtmltoimage.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 set -ex
-sudo add-apt-repository -y ppa:pov/wkhtmltopdf
+
+# sudo add-apt-repository -y ppa:pov/wkhtmltopdf
 sudo apt-get -qq update
-sudo apt-get install -y wkhtmltopdf
+# sudo apt-get install -y wkhtmltopdf
+
+sudo apt-get install -y xfonts-75dpi
+wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2/wkhtmltox-0.12.2_linux-trusty-amd64.deb
+sudo dpkg -i wkhtmltox-0.12.2_linux-trusty-amd64.deb
+wkhtmltoimage http://www.google.com test.png
+ls
+
 wkhtmltopdf -V
 wkhtmltoimage -V
+
 # echo 'exec xvfb-run -a -s "-screen 0 640x480x16" wkhtmltoimage "$@"' | sudo tee /usr/local/bin/wkhtmltoimage.sh >/dev/null
 # sudo chmod a+x /usr/local/bin/wkhtmltoimage.sh
-export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
-sleep 3 # give xvfb some time to start
+
+# export DISPLAY=:99.0
+# sh -e /etc/init.d/xvfb start
+# sleep 3 # give xvfb some time to start
