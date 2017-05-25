e79a8fec2cb84ef4df15904fa7167b3a6bb9f1ee
Michael K. Borregaard <mkborregaard@snm.ku.dk>
2017-05-25 12:05:21 +0200

    variable arg numbers for Base methods

--- src/Plots.jl
+++ src/Plots.jl
@@ -109,7 +109,7 @@ export
 import NaNMath
 # define functions (e.g. `_extrema`, that uses the NaNMath version (which ignores NaNs)) when the type is applicable
 for fun in (:extrema, :minimum, :maximum, :mean)
-       @eval $(Symbol(string("_",fun)))(x) = Base.$(fun)(x)
+       @eval $(Symbol(string("_",fun)))(x...) = Base.$(fun)(x...)
        @eval $(Symbol(string("_",fun))){F <: AbstractFloat}(x::AbstractVector{F}) = NaNMath.$(fun)(x)
 end
 
