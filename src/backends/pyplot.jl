5b3c41bd774f96b6e19aedad119c53df2d77cfb4
AnkurD <ankurd@localhost.localdomain>
2017-09-26 17:22:17 +0900

    Colorbar takes font style from Y Axis

--- src/backends/pyplot.jl
+++ src/backends/pyplot.jl
@@ -999,13 +999,13 @@ function _before_layout_calcs(plt::Plot{PyPlotBackend})
             # create and store the colorbar object (handle) and the axis that it is drawn on.
             # note: the colorbar axis is positioned independently from the subplot axis
             fig = plt.o
-		cbax = fig[:add_axes]([0.8,0.1,0.03,0.8], label = string(gensym()))
-		cb = fig[:colorbar](handle; cax = cbax, kw...)
-		cb[:set_label](sp[:colorbar_title],size=py_dpi_scale(plt, sp[:yaxis][:guidefont].pointsize),family=sp[:yaxis][:guidefont].family)
-		for lab in cb[:ax][:yaxis][:get_ticklabels]()
-			lab[:set_fontsize](py_dpi_scale(plt, sp[:yaxis][:tickfont].pointsize))
-			lab[:set_family](sp[:yaxis][:tickfont].family)
-		end 
+            cbax = fig[:add_axes]([0.8,0.1,0.03,0.8], label = string(gensym()))
+            cb = fig[:colorbar](handle; cax = cbax, kw...)
+            cb[:set_label](sp[:colorbar_title],size=py_dpi_scale(plt, sp[:yaxis][:guidefont].pointsize),family=sp[:yaxis][:guidefont].family)
+            for lab in cb[:ax][:yaxis][:get_ticklabels]()
+                  lab[:set_fontsize](py_dpi_scale(plt, sp[:yaxis][:tickfont].pointsize))
+                  lab[:set_family](sp[:yaxis][:tickfont].family)
+            end 
             sp.attr[:cbar_handle] = cb
             sp.attr[:cbar_ax] = cbax
         end
