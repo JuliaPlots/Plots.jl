e80370361440564ec6495f143e83c060d59c7fde
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-02-09 16:14:05 +0100

    automatically generate gradient

--- PlotThemes/src/juno.jl
+++ PlotThemes/src/juno.jl
@@ -1,3 +1,4 @@
+#inspired by nucleus theme for Atom
 juno_palette = [
     colorant"#FE4365", # red
     colorant"#eca25c", # orange
@@ -22,21 +23,27 @@ _themes[:juno] = PlotTheme(
 @require Juno begin
     if Juno.isactive()
         colors = Juno.syntaxcolors()
-        colors = Dict(k => parse(Colorant, "#"*hex(colors[k])) for (k, v) in colors)
+        colors = Dict(k => parse(Colorant, "#"*hex(colors[k], 6)) for (k, v) in colors)
 
-        #inspired by nucleus theme for Atom
         juno_palette = unique([colors[k] for k in keys(colors) if k ∉ ["background", "variable"]])
 
+        colvec = sort(HSV.(juno_palette), lt=(a,b) -> a.h < b.h)
+        filter!(c -> c.s > 0.5*mean(c -> c.s, colvec), colvec)
+        grad = Vector{eltype(colvec)}(0)
+        for i = 1:length(colvec)-1
+            append!(grad, linspace(colvec[i], colvec[i+1]))
+        end
+
         _themes[:juno] = PlotTheme(
-        bg = colors["background"],
-        bginside = colors["background"],
-        fg = colors["variable"],
-        fgtext = colors["variable"],
-        fgguide = colors["variable"],
-        fglegend = colors["variable"],
-        palette = expand_palette(colors["background"], juno_palette; lchoices=linspace(57,57,1),
-        cchoices=linspace(100,100,1)),
-        gradient = cgrad(:fire).colors
+            bg = colors["background"],
+            bginside = colors["background"],
+            fg = colors["variable"],
+            fgtext = colors["variable"],
+            fgguide = colors["variable"],
+            fglegend = colors["variable"],
+            palette = expand_palette(colors["background"], juno_palette; lchoices=linspace(57,57,1),
+            cchoices=linspace(100,100,1)),
+            gradient = grad
         )
     end
 end
