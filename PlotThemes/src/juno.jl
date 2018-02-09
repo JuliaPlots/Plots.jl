c7454cdac3dfff0b205fab02de78a6784c1f13bc
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-02-09 13:24:47 +0100

    get Juno theme colors from Atom syntax theme

--- PlotThemes/src/juno.jl
+++ PlotThemes/src/juno.jl
@@ -1,10 +1,10 @@
-#inspired by nucleus theme for Atom
 juno_palette = [
     colorant"#FE4365", # red
     colorant"#eca25c", # orange
     colorant"#3f9778", # green
     colorant"#005D7F" # blue
 ]
+
 juno_bg = colorant"#282C34"
 
 _themes[:juno] = PlotTheme(
@@ -18,3 +18,25 @@ _themes[:juno] = PlotTheme(
                                           cchoices=linspace(100,100,1)),
     gradient = cgrad(:fire).colors
 )
+
+@require Juno begin
+    if Juno.isactive()
+        colors = Juno.syntaxcolors()
+        colors = Dict(k => parse(Colorant, "#"*hex(colors[k])) for (k, v) in colors)
+
+        #inspired by nucleus theme for Atom
+        juno_palette = unique([colors[k] for k in keys(colors) if k ∉ ["background", "variable"]])
+
+        _themes[:juno] = PlotTheme(
+        bg = colors["background"],
+        bginside = colors["background"],
+        fg = colors["variable"],
+        fgtext = colors["variable"],
+        fgguide = colors["variable"],
+        fglegend = colors["variable"],
+        palette = expand_palette(colors["background"], juno_palette; lchoices=linspace(57,57,1),
+        cchoices=linspace(100,100,1)),
+        gradient = cgrad(:fire).colors
+        )
+    end
+end
