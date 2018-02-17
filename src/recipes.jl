c0248069a1b250b23d4a244b7eb3adf07fef26ca
Daniel Schwabeneder <daniel.schwabeneder@openmailbox.org>
2018-02-17 23:33:55 +0100

    add Dates.Time recipe

--- src/recipes.jl
+++ src/recipes.jl
@@ -1000,13 +1000,15 @@ abline!(args...; kw...) = abline!(current(), args...; kw...)
 
 
 # -------------------------------------------------
-# Dates
+# Dates & Times
 
 dateformatter(dt) = string(Date(Dates.UTD(dt)))
 datetimeformatter(dt) = string(DateTime(Dates.UTM(dt)))
+timeformatter(t) = string(Dates.Time(Dates.Nanosecond(t)))
 
 @recipe f(::Type{Date}, dt::Date) = (dt -> Dates.value(dt), dateformatter)
 @recipe f(::Type{DateTime}, dt::DateTime) = (dt -> Dates.value(dt), datetimeformatter)
+@recipe f(::Type{Dates.Time}, t::Dates.Time) = (t -> Dates.value(t), timeformatter)
 
 # -------------------------------------------------
 # Complex Numbers
