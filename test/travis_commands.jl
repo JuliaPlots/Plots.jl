91e326695c6b3524cb9b3fd0a6f67659a96b08e2
Ralph A. Smith <smith.ralph.a@gmail.com>
2018-06-23 16:49:16 -0400

    attempt to build w/ nightly on Travis
    conditionals not yet implemented in Pkg for test dependencies.
    may need work on branch checkout for Pkg.develop.

--- test/travis_commands.jl
+++ test/travis_commands.jl
@@ -1,16 +1,16 @@
+import Pkg
 Pkg.add("ImageMagick")
 Pkg.build("ImageMagick")
 
 # Pkg.clone("GR")
 # Pkg.build("GR")
 
-Pkg.clone("https://github.com/JuliaPlots/PlotReferenceImages.jl.git")
-
 # Pkg.clone("https://github.com/JuliaStats/KernelDensity.jl.git")
 
 Pkg.clone("StatPlots")
-Pkg.checkout("PlotUtils","julia0.7")
-Pkg.checkout("RecipesBase","julia0.7")
+
+Pkg.develop("PlotUtils")
+Pkg.develop("RecipesBase")
 
 # Pkg.clone("Blink")
 # Pkg.build("Blink")
