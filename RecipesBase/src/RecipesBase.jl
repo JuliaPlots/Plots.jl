3f5f3e7d13469fd6f00574ab5f770e7fdc46c378
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2018-08-19 22:40:59 +0200

    remove last vestiges of `d`

--- RecipesBase/src/RecipesBase.jl
+++ RecipesBase/src/RecipesBase.jl
@@ -39,7 +39,7 @@ group_as_matrix(t) = false
 # This holds the recipe definitions to be dispatched on
 # the function takes in an attribute dict `d` and a list of args.
 # This default definition specifies the "no-arg" case.
-apply_recipe(d::Dict{Symbol,Any}) = ()
+apply_recipe(plotattributes::Dict{Symbol,Any}) = ()
 
 const _debug_recipes = Bool[false]
 function debug(v::Bool = true)
@@ -50,7 +50,7 @@ end
 
 # this holds the data and attributes of one series, and is returned from apply_recipe
 struct RecipeData
-    d::Dict{Symbol,Any}
+    plotattributes::Dict{Symbol,Any}
     args::Tuple
 end
 
@@ -86,7 +86,7 @@ function get_function_def(func_signature::Expr, args::Vector)
     if func_signature.head == :where
         Expr(:where, get_function_def(front, args), esc.(func_signature.args[2:end])...)
     elseif func_signature.head == :call
-        func = Expr(:call, :(RecipesBase.apply_recipe), esc.([:($PLOTATTRIBUTES::Dict{Symbol, Any}); args])...)
+        func = Expr(:call, :(RecipesBase.apply_recipe), esc.([:(plotattributes::Dict{Symbol, Any}); args])...)
         if isa(front, Expr) && front.head == :curly
             Expr(:where, func, esc.(front.args[2:end])...)
         else
@@ -111,8 +111,8 @@ function create_kw_body(func_signature::Expr)
                 k = k.args[1]
                 @warn("Type annotations on keyword arguments not currently supported in recipes. Type information has been discarded")
             end
-            push!(kw_body.args, :($k = get!($PLOTATTRIBUTES, $(QuoteNode(k)), $v)))
-            push!(cleanup_body.args, :(RecipesBase.is_key_supported($(QuoteNode(k))) || delete!($PLOTATTRIBUTES, $(QuoteNode(k)))))
+            push!(kw_body.args, :($k = get!(plotattributes, $(QuoteNode(k)), $v)))
+            push!(cleanup_body.args, :(RecipesBase.is_key_supported($(QuoteNode(k))) || delete!(plotattributes, $(QuoteNode(k)))))
         end
         args = args[2:end]
     end
@@ -165,10 +165,10 @@ function process_recipe_body!(expr::Expr)
 
                 set_expr = if force
                     # forced override user settings
-                    :($PLOTATTRIBUTES[$k] = $v)
+                    :(plotattributes[$k] = $v)
                 else
                     # if the user has set this keyword, use theirs
-                    :(get!($PLOTATTRIBUTES, $k, $v))
+                    :(get!(plotattributes, $k, $v))
                 end
 
                 expr.args[i] = if quiet
@@ -192,20 +192,6 @@ function process_recipe_body!(expr::Expr)
     end
 end
 
-# PLOTATTRIBUTES_OLD and_replace can be removed after d for accessing plot attributes is
-# is not supported anymore
-const PLOTATTRIBUTES = :plotattributes
-const PLOTATTRIBUTES_OLD = :d
-_replace(ex, old, new) = _replace(ex, 0, old, new)
-_replace(ex, n, old, new) = (ex == old) ? (new, n+1) : (ex, n)
-function _replace(ex::Expr, n, old, new)
-    new_args = []
-    for old_arg in ex.args
-        new_arg, n = _replace(old_arg, n, old, new)
-        push!(new_args, new_arg)
-    end
-    Expr(ex.head, new_args...), n
-end
 # --------------------------------------------------------------------------
 
 """
@@ -257,15 +243,8 @@ number of series to display.  User-defined keyword arguments are passed through,
 - force:   Don't allow user override for this keyword
 """
 macro recipe(funcexpr::Expr)
-    funcexpr, n_depr = _replace(funcexpr, PLOTATTRIBUTES_OLD, PLOTATTRIBUTES)
     func_signature, func_body = funcexpr.args
 
-    if n_depr > 0
-        msg = "Usage of `$PLOTATTRIBUTES_OLD` for accessing plot attributes is deprecated. Found $n_depr usages of `$PLOTATTRIBUTES_OLD` inside `@recipe`. Use `$PLOTATTRIBUTES` instead.\n"
-        deprecation_body = :(Base.depwarn($msg, :d))
-    else
-        deprecation_body = :()
-    end
     if !(funcexpr.head in (:(=), :function))
         error("Must wrap a valid function call!")
     end
@@ -295,7 +274,7 @@ macro recipe(funcexpr::Expr)
         series_list = RecipesBase.RecipeData[]
         func_return = $func_body
         if func_return != nothing
-            push!(series_list, RecipesBase.RecipeData($PLOTATTRIBUTES, RecipesBase.wrap_tuple(func_return)))
+            push!(series_list, RecipesBase.RecipeData(plotattributes, RecipesBase.wrap_tuple(func_return)))
         end
         series_list
     end))
@@ -329,9 +308,9 @@ end
 """
 macro series(expr::Expr)
     esc(quote
-        let $PLOTATTRIBUTES = copy($PLOTATTRIBUTES)
+        let plotattributes = copy(plotattributes)
             args = $expr
-            push!(series_list, RecipesBase.RecipeData($PLOTATTRIBUTES, RecipesBase.wrap_tuple(args)))
+            push!(series_list, RecipesBase.RecipeData(plotattributes, RecipesBase.wrap_tuple(args)))
             nothing
         end
     end)
