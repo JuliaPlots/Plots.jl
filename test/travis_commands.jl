def5be630a217ea9ed60faf984c051cd12f37b6e
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-23 14:18:45 -0700

    Use Pkg3 API in travis_commands.jl

--- test/travis_commands.jl
+++ test/travis_commands.jl
@@ -1,34 +1,29 @@
 using Pkg
 
-Pkg.add("ImageMagick")
+# need this to use Conda
+# ENV["PYTHON"] = ""
+
+Pkg.add([
+    PackageSpec("ImageMagick"),
+    # PackageSpec("GR"),
+    PackageSpec(url="https://github.com/JuliaPlots/PlotReferenceImages.jl.git"),
+    PackageSpec("StatPlots"),
+    # PackageSpec(url="https://github.com/JuliaStats/KernelDensity.jl.git"),
+    PackageSpec(name="PlotUtils", rev="master"),
+    PackageSpec(name="RecipesBase", rev="master"),
+    # PackageSpec(name="Blink", rev="master"),
+    # PackageSpec(name="Rsvg", rev="master"),
+    # PackageSpec(name="PlotlyJS", rev="master"),
+    # PackageSpec(name="VisualRegressionTests", rev="master"),
+    # PackageSpec("PyPlot"),
+    # PackageSpec("InspectDR"),
+])
+
 Pkg.build("ImageMagick")
-
-# Pkg.clone("GR")
 # Pkg.build("GR")
-
-Pkg.checkout("https://github.com/JuliaPlots/PlotReferenceImages.jl.git")
-
-# Pkg.clone("https://github.com/JuliaStats/KernelDensity.jl.git")
-
-# Pkg.add("StatPlots")
-Pkg.checkout("PlotUtils")
-Pkg.checkout("RecipesBase")
-
-# Pkg.clone("Blink")
 # Pkg.build("Blink")
 # import Blink
 # Blink.AtomShell.install()
-# Pkg.add("Rsvg")
-# Pkg.add("PlotlyJS")
-
-# Pkg.checkout("RecipesBase")
-# Pkg.clone("VisualRegressionTests")
-
-# need this to use Conda
-# ENV["PYTHON"] = ""
-# Pkg.add("PyPlot")
 # Pkg.build("PyPlot")
-
-# Pkg.add("InspectDR")
 
 Pkg.test("Plots"; coverage=false)
