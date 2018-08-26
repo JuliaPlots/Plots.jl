b2f2ffcfed428d4f22b3acfe469eb389265c9c99
Daniel Schwabeneder <daschw@disroot.org>
2018-08-26 21:11:23 +0200

    fix tests

--- test/add_packages.jl
+++ test/add_packages.jl
@@ -3,7 +3,7 @@ using Pkg
 # need this to use Conda
 # ENV["PYTHON"] = ""
 
-Pkg.add([
+to_add = [
     PackageSpec(url="https://github.com/JuliaPlots/PlotReferenceImages.jl.git"),
     # PackageSpec(url="https://github.com/JuliaStats/KernelDensity.jl.git"),
     PackageSpec(name="PlotUtils", rev="master"),
@@ -14,7 +14,18 @@ Pkg.add([
     # PackageSpec(name="VisualRegressionTests", rev="master"),
     # PackageSpec("PyPlot"),
     # PackageSpec("InspectDR"),
-])
+]
+
+if isinteractive()
+    append!(to_add, [
+        PackageSpec(name="FileIO"),
+        PackageSpec(name="ImageMagick"),
+        PackageSpec(name="UnicodePlots"),
+        PackageSpec(name="VisualRegressionTests"),
+    ])
+end
+
+Pkg.add(to_add)
 
 Pkg.build("ImageMagick")
 # Pkg.build("GR")
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ include("imgcomp.jl")
 # don't actually show the plots
 Random.seed!(1234)
 default(show=false, reuse=true)
-img_eps = isinteractive() ? 1e-2 : 10e-2
+img_tol = isinteractive() ? 1e-2 : 10e-2
 
 @testset "GR" begin
     ENV["PLOTS_TEST"] = "true"
@@ -14,7 +14,7 @@ img_eps = isinteractive() ? 1e-2 : 10e-2
     @test gr() == Plots.GRBackend()
     @test backend() == Plots.GRBackend()
 
-    image_comparison_facts(:gr, eps=img_eps, skip = [25, 30])
+    image_comparison_facts(:gr, tol=img_tol, skip = [25, 30])
 end
 
 
@@ -22,7 +22,7 @@ end
 #    @test pyplot() == Plots.PyPlotBackend()
 #    @test backend() == Plots.PyPlotBackend()
 #
-#    image_comparison_facts(:pyplot, eps=img_eps)
+#    image_comparison_facts(:pyplot, tol=img_tol)
 #end
 
 @testset "UnicodePlots" begin
@@ -47,7 +47,7 @@ end
 #                 27, # (polar plots) takes very long / not working
 #                 31, # animation (skipped for speed)
 #             ],
-#             eps=img_eps)
+#             tol=img_tol)
 #     end
 # end
 
@@ -71,7 +71,7 @@ end
 #             28, # heatmap not defined
 #             31, # animation
 #         ],
-#         eps=img_eps)
+#         tol=img_tol)
 # end
 
 
@@ -80,7 +80,7 @@ end
 #     @test backend() == Plots.PlotlyBackend()
 #
 #     # # until png generation is reliable on OSX, just test on linux
-#     # @static Sys.islinux() && image_comparison_facts(:plotly, only=[1,3,4,7,8,9,10,11,12,14,15,20,22,23,27], eps=img_eps)
+#     # @static Sys.islinux() && image_comparison_facts(:plotly, only=[1,3,4,7,8,9,10,11,12,14,15,20,22,23,27], tol=img_tol)
 # end
 
 
@@ -89,7 +89,7 @@ end
 #     @test backend() == Plots.ImmerseBackend()
 #
 #     # as long as we can plot anything without error, it should be the same as Gadfly
-#     image_comparison_facts(:immerse, only=[1], eps=img_eps)
+#     image_comparison_facts(:immerse, only=[1], tol=img_tol)
 # end
 
 
@@ -98,7 +98,7 @@ end
 #     @test backend() == Plots.PlotlyJSBackend()
 #
 #     # as long as we can plot anything without error, it should be the same as Plotly
-#     image_comparison_facts(:plotlyjs, only=[1], eps=img_eps)
+#     image_comparison_facts(:plotlyjs, only=[1], tol=img_tol)
 # end
 
 
@@ -111,7 +111,7 @@ end
 #     @test plot(sort(rand(10)), rand(Int, 10, 3)) == not(nothing)
 #     @test plot!(rand(10,3), rand(10,3)) == not(nothing)
 #
-#     image_comparison_facts(:gadfly, skip=[4,6,23,24,27], eps=img_eps)
+#     image_comparison_facts(:gadfly, skip=[4,6,23,24,27], tol=img_tol)
 # end
 
 
