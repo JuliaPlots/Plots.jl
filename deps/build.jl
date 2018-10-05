ee4fc2803b489e9aa9ae30b0d1c409316657ca56
SimonDanisch <sdanisch@gmail.com>
2018-10-05 12:27:01 +0200

    dont download by default

--- deps/build.jl
+++ deps/build.jl
@@ -1,16 +1,13 @@
 
 #TODO: download https://cdn.plot.ly/plotly-latest.min.js to deps/ if it doesn't exist
 file_path = ""
-if get(ENV, "PLOTS_HOST_DEPENDENCY_LOCAL", "true") == "true"
+if get(ENV, "PLOTS_HOST_DEPENDENCY_LOCAL", "false") == "true"
     global file_path
     local_fn = joinpath(dirname(@__FILE__), "plotly-latest.min.js")
     if !isfile(local_fn)
         @info("Cannot find deps/plotly-latest.min.js... downloading latest version.")
         download("https://cdn.plot.ly/plotly-latest.min.js", local_fn)
-        @show local_fn isfile(local_fn)
-        if isfile(local_fn)
-            file_path = local_fn
-        end
+        isfile(local_fn) && (file_path = local_fn)
     else
         file_path = local_fn
     end
