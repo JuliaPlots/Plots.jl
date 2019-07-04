ddaa69bce2ffbc704fe9c35c6c6380793e7cf32b
Daniel Schwabeneder <daschw@disroot.org>
2019-07-04 12:48:47 +0200

    move areaplot doc from shorthands to recipes

--- src/shorthands.jl
+++ src/shorthands.jl
@@ -379,18 +379,6 @@ julia> curves([1,2,3,4],[1,1,2,4])
 """
 @shorthands curves
 
-"""
-    areaplot([x,] y)
-    areaplot!([x,] y)
-
-Draw a stacked area plot of the matrix y.
-# Examples
-```julia-repl
-julia> areaplot(1:3, [1 2 3; 7 8 9; 4 5 6], seriescolor = [:red :green :blue], fillalpha = [0.2 0.3 0.4])
-```
-"""
-@shorthands areaplot
-
 "Plot a pie diagram"
 pie(args...; kw...)        = plot(args...; kw...,  seriestype = :pie, aspect_ratio = :equal, grid=false, xticks=nothing, yticks=nothing)
 pie!(args...; kw...)       = plot!(args...; kw..., seriestype = :pie, aspect_ratio = :equal, grid=false, xticks=nothing, yticks=nothing)
