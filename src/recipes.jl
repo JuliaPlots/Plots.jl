149e915994e8d9e9f0b16b7815464289f952d35a
Daniel Schwabeneder <daschw@disroot.org>
2019-07-04 12:48:47 +0200

    move areaplot doc from shorthands to recipes

--- src/recipes.jl
+++ src/recipes.jl
@@ -1184,7 +1184,16 @@ end
     end
 end
 
+"""
+    areaplot([x,] y)
+    areaplot!([x,] y)
 
+Draw a stacked area plot of the matrix y.
+# Examples
+```julia-repl
+julia> areaplot(1:3, [1 2 3; 7 8 9; 4 5 6], seriescolor = [:red :green :blue], fillalpha = [0.2 0.3 0.4])
+```
+"""
 @userplot AreaPlot
 
 @recipe function f(a::AreaPlot)
