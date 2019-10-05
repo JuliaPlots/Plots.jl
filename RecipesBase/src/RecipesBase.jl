62d4eb6ea0e4475fa5f628d203a46dc59fe13fc8
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-10-05 14:35:51 -0400

    Document the `@shorthands` macro (#56)
    * Update RecipesBase.jl
    
    * properly escape string
    
    this isn't markdown, after all
    
    * properly escape dollar signs

--- RecipesBase/src/RecipesBase.jl
+++ RecipesBase/src/RecipesBase.jl
@@ -361,7 +361,27 @@ end
 
 #----------------------------------------------------------------------------
 
-# define and export shorthand plotting method definitions
+"""
+    @shorthands(funcname::Symbol)
+
+Defines and exports shorthand plotting method definitions (`\$funcname` and `\$funcname!`).
+Pass the series type (as a symbol) to the macro.
+
+## Examples
+
+```julia
+# define some series type
+@recipe function f(::Type{Val{:myseriestype}}, x, y)
+    # some implementation here
+end
+# docstrings are forwarded
+\"\"\"
+    myseriestype(x, y)
+Plot my series type!
+\"\"\"
+@shorthands myseriestype
+```
+"""
 macro shorthands(funcname::Symbol)
     funcname2 = Symbol(funcname, "!")
     esc(quote
