37ff1a73edc9cbbb52ce4d1d8d11cfe6773e8e30
Simon Christ <SimonChrist@gmx.de>
2019-11-12 16:55:13 +0100

    not displaying in Juno

--- src/backends/pgfplotsx.jl
+++ src/backends/pgfplotsx.jl
@@ -1,11 +1,13 @@
+using PGFPlotsX: PGFPlotsX
 # --------------------------------------------------------------------------------------
 # display calls this and then _display
 function _update_plot_object(plt::Plot{PGFPlotsXBackend})
     plt.o = PGFPlotsX.Axis()
+    push!( plt.o,  PGFPlotsX.Plot(PGFPlotsX.Coordinates(1:5,1:5)) )
 end
 
 function _show(io::IO, mime::MIME"image/svg+xml", plt::Plot{PGFPlotsXBackend})
-    plt.o
+    show(io, mime, plt.o)
 end
 
 function _show(io::IO, mime::MIME"application/pdf", plt::Plot{PGFPlotsXBackend})
@@ -13,7 +15,7 @@ function _show(io::IO, mime::MIME"application/pdf", plt::Plot{PGFPlotsXBackend})
 end
 
 function _show(io::IO, mime::MIME"image/png", plt::Plot{PGFPlotsXBackend})
-    display("image/png", plt.o)
+    show(io, mime, plt.o)
 end
 
 function _show(io::IO, mime::MIME"application/x-tex", plt::Plot{PGFPlotsXBackend})
