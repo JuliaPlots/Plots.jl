91d4e9dbe2ab8bab90b65b64fe736f788c778f31
Simon Christ <SimonChrist@gmx.de>
2019-11-12 16:27:27 +0100

    create display methods

--- src/backends/pgfplotsx.jl
+++ src/backends/pgfplotsx.jl
@@ -1,13 +1,25 @@
 # --------------------------------------------------------------------------------------
+# display calls this and then _display
+function _update_plot_object(plt::Plot{PGFPlotsXBackend})
+    plt.o = PGFPlotsX.Axis()
+end
+
 function _show(io::IO, mime::MIME"image/svg+xml", plt::Plot{PGFPlotsXBackend})
+    plt.o
 end
 
 function _show(io::IO, mime::MIME"application/pdf", plt::Plot{PGFPlotsXBackend})
+    show(io, mime, plt.o)
+end
+
+function _show(io::IO, mime::MIME"image/png", plt::Plot{PGFPlotsXBackend})
+    display("image/png", plt.o)
 end
 
 function _show(io::IO, mime::MIME"application/x-tex", plt::Plot{PGFPlotsXBackend})
+    PGFPlotsX.print_tex(plt.o)
 end
 
 function _display(plt::Plot{PGFPlotsXBackend})
-
+    plt.o
 end
