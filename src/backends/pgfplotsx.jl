1e2ea1614c5d5fc79c66e5a68f87cc38a12214bb
Simon Christ <SimonChrist@gmx.de>
2019-11-18 17:16:27 +0100

    add tests

--- src/backends/pgfplotsx.jl
+++ src/backends/pgfplotsx.jl
@@ -567,18 +567,22 @@ function _update_plot_object(plt::Plot{PGFPlotsXBackend})
 end
 
 function _show(io::IO, mime::MIME"image/svg+xml", plt::Plot{PGFPlotsXBackend})
+    _update_plot_object(plt)
     show(io, mime, plt.o)
 end
 
 function _show(io::IO, mime::MIME"application/pdf", plt::Plot{PGFPlotsXBackend})
+    _update_plot_object(plt)
     show(io, mime, plt.o)
 end
 
 function _show(io::IO, mime::MIME"image/png", plt::Plot{PGFPlotsXBackend})
+    _update_plot_object(plt)
     show(io, mime, plt.o)
 end
 
 function _show(io::IO, mime::MIME"application/x-tex", plt::Plot{PGFPlotsXBackend})
+    _update_plot_object(plt)
     PGFPlotsX.print_tex(plt.o)
 end
 
