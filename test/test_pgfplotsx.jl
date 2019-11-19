c0fc671a83ece82f7c95b0ce0c199a74a1faf9fc
Simon Christ <SimonChrist@gmx.de>
2019-11-19 15:31:36 +0100

    status quo

--- test/test_pgfplotsx.jl
+++ test/test_pgfplotsx.jl
@@ -27,4 +27,39 @@ end
             @test pgfx_plot.o.contents[1]["colorbar"] === nothing
         end
      end # testset
+     @testset "Color docs example" begin
+        y = rand(100)
+        plot(0:10:100, rand(11, 4), lab="lines", w=3, palette=:grays, fill=0, α=0.6)
+        scatter!(y, zcolor=abs.(y .- 0.5), m=(:heat, 0.8, Plots.stroke(1, :green)), ms=10 * abs.(y .- 0.5) .+ 4, lab="grad")
+        # TODO: marker size does not adjust
+        # TODO: marker stroke is incorrect
+        # TODO: fill legends
+        # TODO: adjust colorbar limits to data
+     end # testset
+     @testset "Plot in pieces" begin
+        plot(rand(100) / 3, reg=true, fill=(0, :green))
+        scatter!(rand(100), markersize=6, c=:orange)
+        # TODO: legends should be different
+     end # testset
+     @testset "Marker types" begin
+        markers = filter((m->begin
+           m in Plots.supported_markers()
+           end), Plots._shape_keys)
+        markers = reshape(markers, 1, length(markers))
+        n = length(markers)
+        x = (range(0, stop=10, length=n + 2))[2:end - 1]
+        y = repeat(reshape(reverse(x), 1, :), n, 1)
+        scatter(x, y, m=(8, :auto), lab=map(string, markers), bg=:linen, xlim=(0, 10), ylim=(0, 10))
+        #TODO: fix markers that show up as circles
+     end # testset
+     @testset "Layout" begin
+        plot(Plots.fakedata(100, 10), layout=4, palette=[:grays :blues :heat :lightrainbow], bg_inside=[:orange :pink :darkblue :black])
+        # TODO: add layouting
+     end # testset
+     @testset "Polar plots" begin
+        Θ = range(0, stop=1.5π, length=100)
+        r = abs.(0.1 * randn(100) + sin.(3Θ))
+        plot(Θ, r, proj=:polar, m=2)
+        # TODO: handle polar plots
+     end # testset
 end # testset
