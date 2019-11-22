fa324561dacbcf2178f82714123b3326dbaccd6e
Simon Christ <SimonChrist@gmx.de>
2019-11-22 13:50:18 +0100

    restructure

--- test/test_pgfplotsx.jl
+++ test/test_pgfplotsx.jl
@@ -126,4 +126,10 @@ end
         plot( x, y, quiver = (u, v), arrow = true )
         # TODO: could adjust limits to fit arrows if too long, but how?
      end # testset
+     @testset "Annotations" begin
+        y = rand(10)
+        plot(y, annotations=(3, y[3], Plots.text("this is \\#3", :left)), leg=false)
+        annotate!([(5, y[5], Plots.text("this is \\#5", 16, :red, :center)), (10, y[10], Plots.text("this is \\#10", :right, 20, "courier"))])
+        scatter!(range(2, stop=8, length=6), rand(6), marker=(50, 0.2, :orange), series_annotations=["series", "annotations", "map", "to", "series", Plots.text("data", :green)])
+     end # testset
 end # testset
