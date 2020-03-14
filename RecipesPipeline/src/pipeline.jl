4b9cf60759360b9cc810dd708330d445c6d2cb7f
Simon Christ <SimonChrist@gmx.de>
2020-03-14 17:55:06 +0100

    add _recipe function stubs (#8)
    * add _recipe function stubs
    
    * add bangs

--- RecipesPipeline/src/pipeline.jl
+++ RecipesPipeline/src/pipeline.jl
@@ -1,19 +1,25 @@
-
+## Stubs
+function _recipe_init!(plt, plotattributes, args) end
+function _recipe_after_user!(plt, plotattributes, args) end
+function _recipe_after_plot!(plt, plotattributes, args) end
+function _recipe_after_series!(plt, plotattributes, args, series_ind) end
+function _recipe_finish!(plt, plotattributes, args) end
+##
 # Here comes the specification of when which recipe is processed.
 # It contains functions before and after every stage for interaction with the plotting package.
 
-function recipe_pipeline(plt,             # frontend specific representation of a plot
+function recipe_pipeline!(plt,             # frontend specific representation of a plot
                          plotattributes,  # current state of recipe keywords
                          args,            # set of arguments passed by the user
   )
-  _recipe_init(plt, plotattributes, args)
+  _recipe_init!(plt, plotattributes, args)
   kw_list = _process_userrecipes(plt, plotattributes, args)
-  _recipe_after_user(plt, plotattributes, args)
+  _recipe_after_user!(plt, plotattributes, args)
   kw_list = _process_plotrecipes(plt, plotattributes, args)
-  _recipe_after_plot(plt, plotattributes, args)
+  _recipe_after_plot!(plt, plotattributes, args)
   for (series_ind, series) in enumerate(series_list)
     kw_list = _process_seriesrecipe(plt, plotattributes)
-    _recipe_after_series(plt, plotattributes, series_ind)
+    _recipe_after_series!(plt, plotattributes, series_ind)
   end
-  _recipe_finish((plt, plotattributes, args)
+  _recipe_finish!((plt, plotattributes, args)
 end
