a9c659aa21116809b8df77ace881932834efafd8
Daniel Schwabeneder <daschw@disroot.org>
2020-04-23 20:35:32 +0200

    fix errorbars with string axes

--- src/recipes.jl
+++ src/recipes.jl
@@ -1019,7 +1019,8 @@ error_tuple(x::Tuple) = x
 
 function error_coords(errorbar, errordata, otherdata...)
     ed = Vector{float_extended_type(errordata)}(undef, 0)
-    od = [Float64[] for _ in otherdata]
+    od = [Vector{float_extended_type(odi)}(undef, 0) for odi in otherdata]
+    @show eltype(od)
     for (i, edi) in enumerate(errordata)
         for (j, odj) in enumerate(otherdata)
             odi = _cycle(odj, i)
