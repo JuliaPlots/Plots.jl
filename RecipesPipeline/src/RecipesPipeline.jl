f3a52c80bf8d25da5c9b22be24749fd04bf96989
Anshul Singhvi <asinghvi17@simons-rock.edu>
2020-05-08 13:41:32 +0530

    Fix a typo in the `recipe_pipeline!` constructor (#54)

--- RecipesPipeline/src/RecipesPipeline.jl
+++ RecipesPipeline/src/RecipesPipeline.jl
@@ -53,9 +53,9 @@ include("recipes.jl")
     recipe_pipeline!(plt, plotattributes, args)
 
 Recursively apply user recipes, type recipes, plot recipes and series recipes to build a
-list of `Dict`s, each corresponding to a series. At the beginning `plotattributes`
-contains only the keyword arguments passed in by the user. Add all series to the plot
-bject `plt` and return it.
+list of `Dict`s, each corresponding to a series. At the beginning, `plotattributes`
+contains only the keyword arguments passed in by the user. Then, add all series to the plot
+object `plt` and return it.
 """
 function recipe_pipeline!(plt, plotattributes, args)
     plotattributes[:plot_object] = plt
