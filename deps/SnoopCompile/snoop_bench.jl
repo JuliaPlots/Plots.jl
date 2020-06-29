acd6cfabc12f09df957f0dd470a447b84cce3672
Daniel Schwabeneder <daschw@disroot.org>
2020-06-29 18:53:53 +0200

    string instead of versionnumber

--- deps/SnoopCompile/snoop_bench.jl
+++ deps/SnoopCompile/snoop_bench.jl
@@ -4,7 +4,7 @@ snoop_bench(
     BotConfig(
         "Plots",
         else_os = "linux",
-        version = [v"1.3", v"1", "nightly"],
+        version = ["1.3", "1", "nightly"],
         else_version = v"1",
     ),
     joinpath(@__DIR__, "precompile_script.jl"),
