86b92698cc6102c85850541eb4bcae21ee39d65f
Daniel Schwabeneder <daschw@disroot.org>
2020-06-29 18:46:53 +0200

    xvfb-run and update versions

--- deps/SnoopCompile/snoop_bot.jl
+++ deps/SnoopCompile/snoop_bot.jl
@@ -1,6 +1,11 @@
 using SnoopCompile
 
 snoop_bot(
-    BotConfig("Plots", version = [v"1.2", v"1"]),
+    BotConfig(
+        "Plots",
+        else_os = "linux",
+        version = [v"1.3", v"1", "nightly"],
+        else_version = v"1",
+    ),
     joinpath(@__DIR__, "precompile_script.jl"),
 )
