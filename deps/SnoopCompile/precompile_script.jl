4f791a394e252fac17dbb985ee502cef74ed1925
Daniel Schwabeneder <daschw@disroot.org>
2020-06-30 18:37:32 +0200

    don't display plotly plots

--- deps/SnoopCompile/precompile_script.jl
+++ deps/SnoopCompile/precompile_script.jl
@@ -1,4 +1,4 @@
 using Plots
 
 Plots.test_examples(:gr, skip = Plots._backend_skips[:gr])
-Plots.test_examples(:plotly, skip = Plots._backend_skips[:plotly])
+Plots.test_examples(:plotly, skip = Plots._backend_skips[:plotly], disp = false)
