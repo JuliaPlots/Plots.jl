915eafcea0c574442f71a9ce04394c8fb71ef479
Daniel Schwabeneder <daschw@disroot.org>
2020-09-27 22:47:54 +0200

    move operation definitions for pct to PlotMeasures module

--- src/layouts.jl
+++ src/layouts.jl
@@ -4,12 +4,6 @@
 to_pixels(m::AbsoluteLength) = m.value / 0.254
 
 const _cbar_width = 5mm
-
-Base.:*(m1::AbsoluteLength, m2::Length{:pct}) = AbsoluteLength(m1.value * m2.value)
-Base.:*(m1::Length{:pct}, m2::AbsoluteLength) = AbsoluteLength(m2.value * m1.value)
-Base.:/(m1::AbsoluteLength, m2::Length{:pct}) = AbsoluteLength(m1.value / m2.value)
-Base.:/(m1::Length{:pct}, m2::AbsoluteLength) = AbsoluteLength(m2.value / m1.value)
-
 const defaultbox = BoundingBox(0mm, 0mm, 0mm, 0mm)
 
 left(bbox::BoundingBox) = bbox.x0[1]
