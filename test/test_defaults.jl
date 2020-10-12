54f29f283ebb3506667183cbd224fb9fb7d807da
Simon Christ <SimonChrist@gmx.de>
2020-10-12 14:05:44 +0200

    Update test_defaults.jl

--- test/test_defaults.jl
+++ test/test_defaults.jl
@@ -1,8 +1,11 @@
 using Plots, Test
 
 const PLOTS_DEFAULTS = Dict(:theme => :wong2)
+Plots.__init__()
 
 @testset "Loading theme" begin
-    Plots.__init__()
     plot(1:5)[1][1][:series_color] == colorant"black"
 end
+
+empty!(PLOTS_DEFAULTS)
+Plots.__init__()
