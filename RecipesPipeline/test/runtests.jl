9092a3a08b84157bfc93b6ab2a191309ec7bb19f
Daniel Schwabeneder <daschw@disroot.org>
2020-10-28 21:14:26 +0100

    switch to github-actions and update runtests.jl to run Plots test images

--- RecipesPipeline/test/runtests.jl
+++ RecipesPipeline/test/runtests.jl
@@ -1,11 +1,25 @@
+using Distributions
+using FileIO
+using GeometryBasics
+using ImageMagick
+using Images
+using OffsetArrays
+using Plots
+using Random
 using RecipesPipeline
+using RDatasets
+using SparseArrays
+using StaticArrays
+using Statistics
+using StatsPlots
 using Test
 
-makie_test_dir = joinpath(@__DIR__, "test_makie")
-mkpath(makie_test_dir)
-
 @testset "RecipesPipeline.jl" begin
-    @testset "Makie integration" begin cd(makie_test_dir) do
-        include("makie.jl")
-    end end
+    @testset "Plots tests" begin
+    for i in eachindex(Plots._examples)
+        if i ∉ Plots._backend_skips[:gr]
+            @test Plots.test_examples(:gr, i, disp=false) isa Plots.Plot
+        end
+    end
+    end
 end
