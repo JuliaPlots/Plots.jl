059fbdfb6da7fd9d1274faa5a17576bc0c919f3b
Daniel Schwabeneder <daschw@disroot.org>
2020-10-28 22:31:22 +0100

    else_os and else_version

--- deps/SnoopCompile/snoop_bot.jl
+++ deps/SnoopCompile/snoop_bot.jl
@@ -4,6 +4,8 @@ snoop_bot(
     BotConfig(
         "Plots",
         yml_path= "SnoopCompile.yml",
+        else_os = "linux",
+        else_version = "1.5",
     ),
     joinpath(@__DIR__, "precompile_script.jl"),
 )
