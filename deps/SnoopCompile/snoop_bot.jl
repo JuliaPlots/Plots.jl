978ffb799bf5ed1badfee293d119ae4a7ed5ad2d
Daniel Schwabeneder <daschw@disroot.org>
2020-10-28 22:14:17 +0100

    use only yml_path in BotConfig

--- deps/SnoopCompile/snoop_bot.jl
+++ deps/SnoopCompile/snoop_bot.jl
@@ -4,8 +4,6 @@ snoop_bot(
     BotConfig(
         "Plots",
         yml_path= "SnoopCompile.yml",
-        os = "linux",
-        version = "1.5",
     ),
     joinpath(@__DIR__, "precompile_script.jl"),
 )
