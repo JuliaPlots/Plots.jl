22cc594f185ade61d30ef3e003906e1c0cb60cbd
Daniel Schwabeneder <daschw@disroot.org>
2020-10-28 21:33:12 +0100

    keep makie tests

--- RecipesPipeline/test/runtests.jl
+++ RecipesPipeline/test/runtests.jl
@@ -14,7 +14,13 @@ using Statistics
 using StatsPlots
 using Test
 
+# makie_test_dir = joinpath(@__DIR__, "test_makie")
+# mkpath(makie_test_dir)
+
 @testset "RecipesPipeline.jl" begin
+    # @testset "Makie integration" begin cd(makie_test_dir) do
+    #     include("makie.jl")
+    # end end
     @testset "Plots tests" begin
     for i in eachindex(Plots._examples)
         if i ∉ Plots._backend_skips[:gr]
