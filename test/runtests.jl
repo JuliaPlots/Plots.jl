fa48e5c4901d0abdc80c3c0f581791311f2f8502
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2020-12-06 15:20:11 -0500

    Moved Dates tests to be before backend tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -32,10 +32,10 @@ include("test_defaults.jl")
 include("test_axes.jl")
 include("test_axis_letter.jl")
 include("test_components.jl")
+include("integration_dates.jl")
 include("test_recipes.jl")
 include("test_hdf5plots.jl")
 include("test_pgfplotsx.jl")
-include("integration_dates.jl")
 
 reference_dir(args...) = joinpath(homedir(), ".julia", "dev", "PlotReferenceImages", args...)
 
