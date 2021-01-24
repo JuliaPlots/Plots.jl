3c9580fb2faacfd6eeea714555518bb353a925a2
Ian <i.r.butterworth@gmail.com>
2021-01-24 15:45:49 -0500

    fixes

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,8 +4,8 @@ const SUITE = BenchmarkGroup()
 julia_cmd = get(ENV, "TESTCMD", Base.JLOptions().julia_bin)
 
 # numbered to enforce sequence
-SUITE["0_load_plot_display"] = @benchmarkable run(`$(julia_cmd) -e "using Plots; display(plot(1:0.1:10, sin.(1:0.1:10))))"`)
+SUITE["1_load_plot_display"] = @benchmarkable run(`sh -c $("$julia_cmd --startup-file=no -e 'using Plots; display(plot(1:0.1:10, sin.(1:0.1:10))))'")`)
 
-SUITE["1_load"] = @benchmarkable @eval(using Plots)
-SUITE["2_plot"] = @benchmarkable p = plot(1:0.1:10, sin.(1:0.1:10))
-SUITE["3_display"] = @benchmarkable display(p) setup=(p = plot(1:0.1:10, sin.(1:0.1:10)))
+SUITE["2_load"] = @benchmarkable @eval(using Plots)
+SUITE["3_plot"] = @benchmarkable p = plot(1:0.1:10, sin.(1:0.1:10))
+SUITE["4_display"] = @benchmarkable display(p) setup=(p = plot(1:0.1:10, sin.(1:0.1:10)))
