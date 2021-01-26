d538c4387f1164d89a76e738e8e6d098e660dd37
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-01-26 02:47:58 -0600

    CompileBot use default precompile path

--- deps/SnoopCompile/snoop_bench.jl
+++ deps/SnoopCompile/snoop_bench.jl
@@ -3,9 +3,6 @@ using CompileBot
 snoop_bench(
     BotConfig(
         "Plots",
-        yml_path= "SnoopCompile.yml",
-        else_os = "linux",
-        else_version = "1.5",
     ),
     joinpath(@__DIR__, "precompile_script.jl"),
 )
