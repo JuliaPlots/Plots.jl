49eb4002b1de51bc6c61e0f1d4a7e9c10d3c500f
daschw <daschw@users.noreply.github.com>
2021-01-26 10:45:56 +0000

    Update precompile_*.jl file

---
+++ src/precompile_includer.jl
@@ -0,0 +1,17 @@
+should_precompile = true
+
+
+# Don't edit the following! Instead change the script for `snoop_bot`.
+ismultios = false
+ismultiversion = false
+# precompile_enclosure
+@static if !should_precompile
+    # nothing
+elseif !ismultios && !ismultiversion
+    @static if (isfile("../deps/SnoopCompile/precompile/precompile_Plots.jl"))
+        include("../deps/SnoopCompile/precompile/precompile_Plots.jl")
+        _precompile_()
+    end
+else
+
+end # precompile_enclosure
