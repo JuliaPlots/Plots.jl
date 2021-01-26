a53f4e0fb9219d5da3e293f417fd65a38afc410d
daschw <daschw@users.noreply.github.com>
2021-01-26 09:04:03 +0000

    Update precompile_*.jl file

--- src/precompile_includer.jl
+++ src/precompile_includer.jl
@@ -1,3 +1,44 @@
-include("../deps/SnoopCompile/precompile/linux/1.5/precompile_Plots.jl")
+should_precompile = true
 
-_precompile_()
+
+# Don't edit the following! Instead change the script for `snoop_bot`.
+ismultios = true
+ismultiversion = true
+# precompile_enclosure
+@static if !should_precompile
+    # nothing
+elseif !ismultios && !ismultiversion
+    @static if (isfile("../deps/SnoopCompile/precompile/precompile_Plots.jl"))
+        include("../deps/SnoopCompile/precompile/precompile_Plots.jl")
+        _precompile_()
+    end
+else
+    @static if Sys.islinux()
+        @static if v"1.5.0-DEV" <= VERSION <= v"1.5.9"
+            @static if (isfile("../deps/SnoopCompile/precompile/linux/1.5/precompile_Plots.jl"))
+                include("../deps/SnoopCompile/precompile/linux/1.5/precompile_Plots.jl")
+                _precompile_()
+            end
+        else
+            @static if (isfile("../deps/SnoopCompile/precompile/linux/1.5/precompile_Plots.jl"))
+                include("../deps/SnoopCompile/precompile/linux/1.5/precompile_Plots.jl")
+                _precompile_()
+            end
+        end
+
+    else
+        @static if v"1.5.0-DEV" <= VERSION <= v"1.5.9"
+            @static if (isfile("../deps/SnoopCompile/precompile/linux/1.5/precompile_Plots.jl"))
+                include("../deps/SnoopCompile/precompile/linux/1.5/precompile_Plots.jl")
+                _precompile_()
+            end
+        else
+            @static if (isfile("../deps/SnoopCompile/precompile/linux/1.5/precompile_Plots.jl"))
+                include("../deps/SnoopCompile/precompile/linux/1.5/precompile_Plots.jl")
+                _precompile_()
+            end
+        end
+
+    end
+
+end # precompile_enclosure
