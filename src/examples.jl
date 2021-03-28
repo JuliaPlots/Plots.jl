ce778ff7be178b542d3604e58ee2aa26fc1738e8
Daniel Schwabeneder <daschw@disroot.org>
2021-03-28 23:20:09 +0200

    add test image

--- src/examples.jl
+++ src/examples.jl
@@ -1138,6 +1138,30 @@ const _examples = PlotExample[
             ),
         ],
     ),
+    PlotExample( # 54
+        "Guide positions and alignment",
+        "",
+        [
+            :(
+              begin
+                plot(
+                    rand(10, 4),
+                    layout=4,
+                    xguide="x guide",
+                    yguide="y guide",
+                    xguidefonthalign=[:left :right :right :left],
+                    yguidefontvalign=[:top :bottom :bottom :top],
+                    xguideposition=:top,
+                    yguideposition=[:right :left :right :left],
+                    ymirror=[false true true false],
+                    xmirror=[false false true true],
+                    legend=false,
+                    seriestype=[:bar :scatter :path :stepmid]
+                )
+              end
+            ),
+        ],
+    ),
 ]
 
 # Some constants for PlotDocs and PlotReferenceImages
