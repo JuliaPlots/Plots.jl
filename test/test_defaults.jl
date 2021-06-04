e5b6ecee14bfcf08ad1aabb3351912a6e91576fa
Simon Christ <SimonChrist@gmx.de>
2021-06-04 14:28:52 +0200

    Update test_defaults.jl

--- test/test_defaults.jl
+++ test/test_defaults.jl
@@ -4,7 +4,7 @@ const PLOTS_DEFAULTS = Dict(:theme => :wong2, :fontfamily => :palantino)
 Plots.__init__()
 
 @testset "Loading theme" begin
-    pl = plot(1:5)plot(1:5)
+    pl = plot(1:5)
     @test pl[1][1][:seriescolor] == RGBA(colorant"black")
     @test guidefont(pl[1][:xaxis]).family == "palantino"
 end
