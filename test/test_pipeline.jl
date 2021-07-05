9f4e32aa4db2d534534634cddb3995e8626c0454
t-bltg <t-bltg@users.noreply.github.com>
2021-07-05 22:29:38 +0200

    add regression test

--- test/test_pipeline.jl
+++ test/test_pipeline.jl
@@ -8,3 +8,11 @@ using Plots, Test
     plot!(pl, tex_output_standalone = true)
     @test pl[:tex_output_standalone] == true
 end
+
+@testset "get_axis_limits" begin
+    x = [.1, 5]
+    p1 = plot(x, [5, .1], yscale=:log10)
+    p2 = plot!(identity)
+    @test all(RecipesPipeline.get_axis_limits(p1, :x) .== x)
+    @test all(RecipesPipeline.get_axis_limits(p2, :x) .== x)
+end
