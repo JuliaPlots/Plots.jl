40060beb8f3f8d2f55339a82f0055ab022073b29
t-bltg <tf.bltg@gmail.com>
2021-08-30 22:52:26 +0200

    v0.4.0 (#102) [skip ci]

--- RecipesPipeline/src/plot_recipe.jl
+++ RecipesPipeline/src/plot_recipe.jl
@@ -27,28 +27,20 @@ function _process_plotrecipe(plt, kw, kw_list, still_to_process)
         push!(kw_list, kw)
         return
     end
-    try
-        st = kw[:seriestype]
-        st = kw[:seriestype] = type_alias(plt, st)
-        datalist = RecipesBase.apply_recipe(kw, Val{st}, plt)
-        if !isnothing(datalist)
-            warn_on_recipe_aliases!(plt, datalist, :plot, st)
-            for data in datalist
-                preprocess_attributes!(plt, data.plotattributes)
-                if data.plotattributes[:seriestype] == st
-                    error("Plot recipe $st returned the same seriestype: $(data.plotattributes)")
-                end
-                push!(still_to_process, data.plotattributes)
+    st = kw[:seriestype]
+    st = kw[:seriestype] = type_alias(plt, st)
+    datalist = RecipesBase.apply_recipe(kw, Val{st}, plt)
+    if !isnothing(datalist)
+        warn_on_recipe_aliases!(plt, datalist, :plot, st)
+        for data in datalist
+            preprocess_attributes!(plt, data.plotattributes)
+            if data.plotattributes[:seriestype] == st
+                error("Plot recipe $st returned the same seriestype: $(data.plotattributes)")
             end
-        else
-            push!(kw_list, kw)
+            push!(still_to_process, data.plotattributes)
         end
-    catch err
-        if err isa MethodError && occursin("plot recipe", err.args)
-            push!(kw_list, kw)
-        else
-            rethrow()
-        end
+    else
+        push!(kw_list, kw)
     end
     return
 end
