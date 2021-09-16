8d72e9d7ec0004561b467aad4982414182ba1dd6
t-bltg <tf.bltg@gmail.com>
2021-09-16 22:30:19 +0200

    Update args.jl

--- src/args.jl
+++ src/args.jl
@@ -428,7 +428,7 @@ const _plot_defaults = KW(
     :dpi                 => DPI,        # dots per inch for images, etc
     :thickness_scaling   => 1,
     :display_type        => :auto,
-    :warn_on_unsupported => false,
+    :warn_on_unsupported => true,
     :extra_plot_kwargs   => Dict(),
     :extra_kwargs        => :series,    # directs collection of extra_kwargs
 )
