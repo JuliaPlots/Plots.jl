b47bcea6e6a96dde087a727f11468241da904aae
t-bltg <tf.bltg@gmail.com>
2021-12-01 23:10:57 +0100

    revert to supporting 1.5 - 1.7

--- deps/SnoopCompile/snoop_bot_config.jl
+++ deps/SnoopCompile/snoop_bot_config.jl
@@ -2,6 +2,6 @@ using CompileBot
 
 botconfig = BotConfig(
     "Plots",
-    version = [v"1.6"],
-    else_version = v"1.7",
+    version = [v"1.5", v"1.6", v"1.7"],  # NOTE: keep in sync with .github/workflows/SnoopCompile.yml
+    # else_version = v"1.7",
 )
