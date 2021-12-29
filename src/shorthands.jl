30d2f01d9d35a4d713039b6839ed2193ef07c560
Diego Javier Zea <diegozea@gmail.com>
2021-12-29 13:30:00 +0100

    Update boxplot doc (#4014)
    Update and improve whisker_width documentation (boxplots).

--- src/shorthands.jl
+++ src/shorthands.jl
@@ -351,7 +351,7 @@ Make a box and whisker plot.
 - `whisker_range`: Real. Whiskers extend `whisker_range`*IQR below the first quartile
            and above the third quartile. Values outside this range are shown as outliers (1.5)
 - `outliers`: Bool. Show outliers? (true)
-- `whisker_width`: Real or Symbol. Length of whiskers (:match)
+- `whisker_width`: Real or Symbol. Length of whiskers; the options are `:match` to match the box width, `:half`, or a number to indicate the total length. (:half)
 
 # Example
 ```julia-repl
