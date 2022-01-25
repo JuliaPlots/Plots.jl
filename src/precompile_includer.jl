be1aa419a6ece914ea361f1f6f397b3ddee070ce
BeastyBlacksmith <BeastyBlacksmith@users.noreply.github.com>
2022-01-25 16:55:04 +0000

    Update precompile_*.jl file [skip ci]

--- src/precompile_includer.jl
+++ src/precompile_includer.jl
@@ -1,5 +1,5 @@
 #! format: off
-should_precompile = true
+should_precompile =     true
 
 
 # Don't edit the following! Instead change the script for `snoop_bot`.
@@ -14,17 +14,17 @@ elseif !ismultios && !ismultiversion
         _precompile_()
     end
 else
-    @static if v"1.6.0-DEV" <= VERSION <= v"1.6.9"
-        @static if isfile(joinpath(@__DIR__, "../deps/SnoopCompile/precompile//1.6/precompile_Plots.jl"))
-            include("../deps/SnoopCompile/precompile//1.6/precompile_Plots.jl")
-            _precompile_()
-        end
-    elseif v"1.7.0-DEV" <= VERSION <= v"1.7.9" 
-        @static if isfile(joinpath(@__DIR__, "../deps/SnoopCompile/precompile//1.7/precompile_Plots.jl"))
-            include("../deps/SnoopCompile/precompile//1.7/precompile_Plots.jl")
-            _precompile_()
-        end
-    else 
+    @static if v"1.6.0-DEV" <= VERSION <= v"1.6.9" 
+    @static if isfile(joinpath(@__DIR__, "../deps/SnoopCompile/precompile//1.6/precompile_Plots.jl"))
+        include("../deps/SnoopCompile/precompile//1.6/precompile_Plots.jl")
+        _precompile_()
+    end
+elseif v"1.7.0-DEV" <= VERSION <= v"1.7.9" 
+    @static if isfile(joinpath(@__DIR__, "../deps/SnoopCompile/precompile//1.7/precompile_Plots.jl"))
+        include("../deps/SnoopCompile/precompile//1.7/precompile_Plots.jl")
+        _precompile_()
+    end
+else 
     end
 
 end # precompile_enclosure
