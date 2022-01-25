a8fea917e2b67fc1c861de8afa69659aa2456cb6
Simon Christ <simonchrist@gmx.de>
2022-01-25 17:08:22 +0100

    format file

--- test/test_pipeline.jl
+++ test/test_pipeline.jl
@@ -25,9 +25,12 @@ end
     for i in axes(data4, 1)
         for attribute in (:fillrange, :ribbon)
             @test plot(data4; NamedTuple{tuple(attribute)}(0)...)[1][i][attribute] == 0
-            @test plot(data4; NamedTuple{tuple(attribute)}(Ref([1, 2]))...)[1][i][attribute] == [1.0, 2.0]
-            @test plot(data4; NamedTuple{tuple(attribute)}(Ref([1 2]))...)[1][i][attribute] == (iseven(i) ? 2 : 1)
-            @test plot(data4; NamedTuple{tuple(attribute)}(Ref(mat))...)[1][i][attribute] == [2(i-1)+1, 2i]
+            @test plot(data4; NamedTuple{tuple(attribute)}(Ref([1, 2]))...)[1][i][attribute] ==
+                  [1.0, 2.0]
+            @test plot(data4; NamedTuple{tuple(attribute)}(Ref([1 2]))...)[1][i][attribute] ==
+                  (iseven(i) ? 2 : 1)
+            @test plot(data4; NamedTuple{tuple(attribute)}(Ref(mat))...)[1][i][attribute] ==
+                  [2(i - 1) + 1, 2i]
         end
     end
 end
