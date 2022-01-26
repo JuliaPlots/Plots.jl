5d5239834c3d41a084f2ecc135e4a43840cd8e67
Tom Gillam <tpgillam@googlemail.com>
2022-01-26 12:21:46 +0000

    Example for vspan and hspan (#4062)
    * Example for vspan and hspan
    
    * Format
    
    * First time contributor

--- src/examples.jl
+++ src/examples.jl
@@ -1264,6 +1264,19 @@ const _examples = PlotExample[
             ),
         ],
     ),
+    PlotExample( # 57
+        "Vertical and horizonal spans",
+        """
+        `vspan` and `hspan` can be used to shade horizontal and vertical ranges.
+        """,
+        [:(
+            begin
+                hspan([1, 2, 3, 4]; label = "hspan", legend = :topleft)
+                vspan!([2, 3]; alpha = 0.5, label = "vspan")
+                plot!([0, 2, 3, 5], [-1, 3, 2, 6]; c = :black, lw = 2, label = "line")
+            end
+        )],
+    ),
 ]
 
 # Some constants for PlotDocs and PlotReferenceImages
