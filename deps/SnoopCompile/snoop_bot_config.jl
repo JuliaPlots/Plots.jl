264ddebd1e8f98789c12cff4606762f341cc7c70
t-bltg <tf.bltg@gmail.com>
2022-01-28 11:50:44 +0100

    enable precompilation on nightly

--- deps/SnoopCompile/snoop_bot_config.jl
+++ deps/SnoopCompile/snoop_bot_config.jl
@@ -2,6 +2,6 @@ using CompileBot
 
 botconfig = BotConfig(
     "Plots",
-    version = [v"1.6", v"1.7"],  # <<< keep versions in sync with .github/workflows/SnoopCompile.yml
-    # else_version = v"1.8",
+    version = [v"1.6", v"1.7"],  # <<< keep versions in sync with .github/workflows/CompileBot.yml
+    else_version = "nightly",
 )
