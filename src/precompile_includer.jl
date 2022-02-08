8959c47891938cc297a1f63d67f97c963b056db5
t-bltg <tf.bltg@gmail.com>
2022-02-08 10:49:22 +0100

    temporarily disable nightly precompilation until #4079 is fixed

--- src/precompile_includer.jl
+++ src/precompile_includer.jl
@@ -1,5 +1,4 @@
-#! format: off
-should_precompile =   true
+should_precompile =  false
 
 
 # Don't edit the following! Instead change the script for `snoop_bot`.
@@ -22,11 +21,6 @@ else
 elseif v"1.7.0-DEV" <= VERSION <= v"1.7.9" 
     @static if isfile(joinpath(@__DIR__, "../deps/SnoopCompile/precompile//1.7/precompile_Plots.jl"))
         include("../deps/SnoopCompile/precompile//1.7/precompile_Plots.jl")
-        _precompile_()
-    end
-elseif v"1.8.0-DEV" <= VERSION <= v"1.8.9" 
-    @static if isfile(joinpath(@__DIR__, "../deps/SnoopCompile/precompile//1.8/precompile_Plots.jl"))
-        include("../deps/SnoopCompile/precompile//1.8/precompile_Plots.jl")
         _precompile_()
     end
 else 
