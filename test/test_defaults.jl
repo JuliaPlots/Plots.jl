a3451ab507ad9e7c0351f0cca3054d77e7e0e11f
Simon Christ <simonchrist@gmx.de>
2022-02-25 15:31:52 +0100

    revert legendfont being determant

--- test/test_defaults.jl
+++ test/test_defaults.jl
@@ -1,4 +1,4 @@
-using Plots, Test, Colors
+using Plots, Test, Plots.Colors
 
 const PLOTS_DEFAULTS = Dict(:theme => :wong2, :fontfamily => :palantino)
 Plots.__init__()
@@ -93,4 +93,9 @@ end # testset
     # match mechanism
     @test sp[:legend_font_color] == sp[:foreground_color_subplot]
     @test Plots.legendfont(sp).color == sp[:foreground_color_subplot]
+
+    # magic invocation
+    @test_nowarn sp = plot(;legendfont = 12)[1]
+    @test sp[:legend_font_pointsize] == 12
+    @test Plots.legendfont(sp).pointsize == 12
 end # testset
