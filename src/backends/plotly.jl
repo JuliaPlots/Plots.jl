20afedd7df02d06bd87f6585041d20252b5e9c1f
Steve Leung <steve@orchardultrasound.com>
2022-03-28 20:45:32 -0700

    Renamed plotly_hover!() to plotly_adjust_hover_label!()

--- src/backends/plotly.jl
+++ src/backends/plotly.jl
@@ -752,7 +752,7 @@ function plotly_series(plt::Plot, series::Series)
     end
 
     plotly_polar!(plotattributes_out, series)
-    plotly_hover!(plotattributes_out, series[:hover])
+    plotly_adjust_hover_label!(plotattributes_out, series[:hover])
 
     return [plotattributes_out]
 end
@@ -807,7 +807,7 @@ function plotly_series_shapes(plt::Plot, series::Series, clims)
         end
         plotattributes_out[:showlegend] = k == 1 ? should_add_to_legend(series) : false
         plotly_polar!(plotattributes_out, series)
-        plotly_hover!(plotattributes_out, _cycle(series[:hover], i))
+        plotly_adjust_hover_label!(plotattributes_out, _cycle(series[:hover], i))
         plotattributes_outs[k] = plotattributes_out
     end
     if series[:fill_z] !== nothing
@@ -931,7 +931,7 @@ function plotly_series_segments(series::Series, plotattributes_base::KW, x, y, z
         end
 
         plotly_polar!(plotattributes_out, series)
-        plotly_hover!(plotattributes_out, _cycle(series[:hover], rng))
+        plotly_adjust_hover_label!(plotattributes_out, _cycle(series[:hover], rng))
 
         if hasfillrange
             # if hasfillrange is true, return two dictionaries (one for original
@@ -1019,7 +1019,7 @@ function plotly_polar!(plotattributes_out::KW, series::Series)
     end
 end
 
-function plotly_hover!(plotattributes_out::KW, hover)
+function plotly_adjust_hover_label!(plotattributes_out::KW, hover)
     if isnothing(hover)
         return nothing
     elseif all(in([:none, false]), hover)
