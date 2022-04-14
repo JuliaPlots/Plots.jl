d9d70be346bf249f2e1b4b17f4e3cedc1b1f61c2
t-bltg <tf.bltg@gmail.com>
2022-04-14 08:23:16 +0200

    try 1.8

--- deps/SnoopCompile/snoop_bot_config.jl
+++ deps/SnoopCompile/snoop_bot_config.jl
@@ -2,6 +2,6 @@ using CompileBot
 
 botconfig = BotConfig(
     "Plots",
-    version = ["1.6", "1.7", "nightly"],  # <<< keep these versions in sync with .github/workflows/SnoopCompile.yml
+    version = ["1.6", "1.7", "1.8", "nightly"],  # <<< keep these versions in sync with .github/workflows/SnoopCompile.yml
     # else_version = "nightly",
 )
