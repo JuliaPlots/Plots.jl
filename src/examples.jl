35247d42281cb3afba83e68f8e94e8a961c955e2
Simon Christ <SimonChrist@gmx.de>
2022-04-19 15:36:07 +0200

    format files

--- src/examples.jl
+++ src/examples.jl
@@ -1278,11 +1278,18 @@ const _examples = PlotExample[
         """
         `areaplot` draws stacked area plots.
         """,
-        [:(
-            begin
-                areaplot(1:3, [1 2 3; 7 8 9; 4 5 6], seriescolor = [:red :green :blue], fillalpha = [0.2 0.3 0.4])
-            end
-        )],
+        [
+            :(
+                begin
+                    areaplot(
+                        1:3,
+                        [1 2 3; 7 8 9; 4 5 6],
+                        seriescolor = [:red :green :blue],
+                        fillalpha = [0.2 0.3 0.4],
+                    )
+                end
+            ),
+        ],
     ),
 ]
 
