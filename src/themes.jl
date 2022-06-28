52fb3740d4a64083783b0917d5015f48b12b5add
Diego Javier Zea <diegozea@gmail.com>
2022-06-28 16:09:33 +0200

    Use spaces rather than tabs

--- src/themes.jl
+++ src/themes.jl
@@ -4,13 +4,13 @@
 Specify the colour theme for plots.
 """
 function theme(s::Symbol; kw...)
-	defaults = if haskey(PlotThemes._themes, s)
-		copy(PlotThemes._themes[s].defaults)
-	else
-		@warn ":$s is not a known theme, using :default"
-		Dict{Symbol, Any}()
-	end
-	_theme(s, defaults; kw...)
+    defaults = if haskey(PlotThemes._themes, s)
+        copy(PlotThemes._themes[s].defaults)
+    else
+        @warn ":$s is not a known theme, using :default"
+        Dict{Symbol, Any}()
+    end
+    _theme(s, defaults; kw...)
 end
 
 function _theme(s::Symbol, defaults::AKW; kw...)
