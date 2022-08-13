80adf98faa675cc9f70f3c7ab5abc6607d90aa05
Brian Sinquin <brian.sinquin@gmail.com>
2022-08-13 15:31:38 +0200

    savefig now Path-Type agnostic

--- src/output.jl
+++ src/output.jl
@@ -1,83 +1,83 @@
 
 defaultOutputFormat(plt::Plot) = "png"
 
-png(plt::Plot, fn::AbstractString) =
+png(plt::Plot, fn) =
     open(addExtension(fn, "png"), "w") do io
         show(io, MIME("image/png"), plt)
     end
-png(fn::AbstractString) = png(current(), fn)
+png(fn) = png(current(), fn)
 
 png(plt::Plot, io::IO) = show(io, MIME("image/png"), plt)
 png(io::IO) = png(current(), io)
 
-svg(plt::Plot, fn::AbstractString) =
+svg(plt::Plot, fn) =
     open(addExtension(fn, "svg"), "w") do io
         show(io, MIME("image/svg+xml"), plt)
     end
-svg(fn::AbstractString) = svg(current(), fn)
+svg(fn) = svg(current(), fn)
 
 svg(plt::Plot, io::IO) = show(io, MIME("image/svg+xml"), plt)
 svg(io::IO) = svg(current(), io)
 
-pdf(plt::Plot, fn::AbstractString) =
+pdf(plt::Plot, fn) =
     open(addExtension(fn, "pdf"), "w") do io
         show(io, MIME("application/pdf"), plt)
     end
-pdf(fn::AbstractString) = pdf(current(), fn)
+pdf(fn) = pdf(current(), fn)
 
 pdf(plt::Plot, io::IO) = show(io, MIME("application/pdf"), plt)
 pdf(io::IO) = pdf(current(), io)
 
-ps(plt::Plot, fn::AbstractString) =
+ps(plt::Plot, fn) =
     open(addExtension(fn, "ps"), "w") do io
         show(io, MIME("application/postscript"), plt)
     end
-ps(fn::AbstractString) = ps(current(), fn)
+ps(fn) = ps(current(), fn)
 
 ps(plt::Plot, io::IO) = show(io, MIME("application/postscript"), plt)
 ps(io::IO) = ps(current(), io)
 
-eps(plt::Plot, fn::AbstractString) =
+eps(plt::Plot, fn) =
     open(addExtension(fn, "eps"), "w") do io
         show(io, MIME("image/eps"), plt)
     end
-eps(fn::AbstractString) = eps(current(), fn)
+eps(fn) = eps(current(), fn)
 
 eps(plt::Plot, io::IO) = show(io, MIME("image/eps"), plt)
 eps(io::IO) = eps(current(), io)
 
-tex(plt::Plot, fn::AbstractString) =
+tex(plt::Plot, fn) =
     open(addExtension(fn, "tex"), "w") do io
         show(io, MIME("application/x-tex"), plt)
     end
-tex(fn::AbstractString) = tex(current(), fn)
+tex(fn) = tex(current(), fn)
 
 tex(plt::Plot, io::IO) = show(io, MIME("application/x-tex"), plt)
 tex(io::IO) = tex(current(), io)
 
-json(plt::Plot, fn::AbstractString) =
+json(plt::Plot, fn) =
     open(addExtension(fn, "json"), "w") do io
         show(io, MIME("application/vnd.plotly.v1+json"), plt)
     end
-json(fn::AbstractString) = json(current(), fn)
+json(fn) = json(current(), fn)
 
 json(plt::Plot, io::IO) = show(io, MIME("application/vnd.plotly.v1+json"), plt)
 json(io::IO) = json(current(), io)
 
-html(plt::Plot, fn::AbstractString) =
+html(plt::Plot, fn) =
     open(addExtension(fn, "html"), "w") do io
         show(io, MIME("text/html"), plt)
     end
-html(fn::AbstractString) = html(current(), fn)
+html(fn) = html(current(), fn)
 
 html(plt::Plot, io::IO) = show(io, MIME("text/html"), plt)
 html(io::IO) = html(current(), io)
 
-txt(plt::Plot, fn::AbstractString; color::Bool = true) =
+txt(plt::Plot, fn; color::Bool = true) =
     open(addExtension(fn, "txt"), "w") do io
         show(IOContext(io, :color => color), MIME("text/plain"), plt)
     end
-txt(fn::AbstractString) = txt(current(), fn)
+txt(fn) = txt(current(), fn)
 
 txt(plt::Plot, io::IO) = show(io, MIME("text/plain"), plt)
 txt(io::IO) = txt(current(), io)
@@ -106,10 +106,17 @@ end
 
 const _extension_map = Dict("tikz" => "tex")
 
-function addExtension(fn::AbstractString, ext::AbstractString)
+"""
+    addExtension(filepath, extension)
+
+Change filepath extension according to the extension map
+"""
+function addExtension(fp, ext::AbstractString)
+    parent = dirname(fp)
+    fn = basename(fp)
     oldfn, oldext = splitext(fn)
     oldext = chop(oldext, head = 1, tail = 0)
-    get(_extension_map, oldext, oldext) == ext ? fn : string(fn, ".", ext)
+    get(_extension_map, oldext, oldext) == ext ? fp : joinpath(parent, string(fn, ".", ext))
 end
 
 """
@@ -119,7 +126,7 @@ Save a Plot (the current plot if `plot` is not passed) to file. The file
 type is inferred from the file extension. All backends support png and pdf
 file types, some also support svg, ps, eps, html and tex.
 """
-function savefig(plt::Plot, fn::AbstractString)
+function savefig(plt::Plot, fn)
     fn = abspath(expanduser(fn))
 
     # get the extension
@@ -135,7 +142,7 @@ function savefig(plt::Plot, fn::AbstractString)
         error("Invalid file extension: ", fn)
     end
 end
-savefig(fn::AbstractString) = savefig(current(), fn)
+savefig(fn) = savefig(current(), fn)
 
 # ---------------------------------------------------------
 
