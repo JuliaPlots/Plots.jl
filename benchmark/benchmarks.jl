0baaaf613675dc5db300b95e8b6b16342b684bce
Simon Christ <christ@cell.uni-hannover.de>
2022-08-23 15:30:46 +0200

    strip down benchmark ci, fix benchmarks.jl local use

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -2,7 +2,7 @@ using BenchmarkTools
 using Plots
 
 const SUITE = BenchmarkGroup()
-julia_cmd = split(get(ENV, "TESTCMD", Base.JLOptions().julia_bin))
+julia_cmd = split(get(ENV, "TESTCMD", unsafe_string(Base.JLOptions().julia_bin)))
 
 SUITE["load_plot_display"] = @benchmarkable run(`$julia_cmd --startup-file=no --project -e 'using Plots; display(plot(1:0.1:10, sin.(1:0.1:10)))'`)
 SUITE["load"] = @benchmarkable run(`$julia_cmd --startup-file=no --project -e 'using Plots'`)
