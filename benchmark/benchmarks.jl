2efb7f29e5295c2a7452964d28ec771b71a0c8d8
Simon Christ <christ@cell.uni-hannover.de>
2022-08-24 10:58:51 +0200

    update benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,7 +4,10 @@ using Plots
 const SUITE = BenchmarkGroup()
 julia_cmd = split(get(ENV, "TESTCMD", unsafe_string(Base.JLOptions().julia_bin)))
 
-SUITE["load_plot_display"] = @benchmarkable run(`$julia_cmd --startup-file=no --project -e 'using Plots; display(plot(1:0.1:10, sin.(1:0.1:10)))'`)
-SUITE["load"] = @benchmarkable run(`$julia_cmd --startup-file=no --project -e 'using Plots'`)
+SUITE["load_plot_display"] = @benchmarkable begin
+    @eval using Plots
+    display(plot(1:0.1:10, sin.(1:0.1:10)))
+end
+SUITE["load"] = @benchmarkable @eval using Plots
 SUITE["plot"] = @benchmarkable p = plot(1:0.1:10, sin.(1:0.1:10)) samples=1 evals=1
 SUITE["display"] = @benchmarkable display(p) setup=(p = plot(1:0.1:10, sin.(1:0.1:10))) samples=1 evals=1
