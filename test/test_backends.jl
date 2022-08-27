c7e903c73b5e14b1ff29c968db59b86017a297d4
t-bltg <tf.bltg@gmail.com>
2022-08-27 13:14:29 +0200

    relax test tolerance for `1.8`

--- test/test_backends.jl
+++ test/test_backends.jl
@@ -3,7 +3,7 @@ is_ci() = get(ENV, "CI", "false") == "true"
 const TEST_MODULE = Module(:PlotsTestModule)
 const PLOTS_IMG_TOL = parse(
     Float64,
-    get(ENV, "PLOTS_IMG_TOL", is_ci() ? (Sys.iswindows() ? "2e-3" : "1e-4") : "1e-5"),
+    get(ENV, "PLOTS_IMG_TOL", is_ci() ? "1e-3" : "1e-5"),  # (Sys.iswindows() ? "2e-3" : "1e-4")
 )
 
 Base.eval(TEST_MODULE, quote
