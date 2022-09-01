5f52dd4b44798044c2cb7d262b5a5565f2a6ed5c
t-bltg <tf.bltg@gmail.com>
2022-09-02 00:29:56 +0200

    restrict img tol

--- test/test_backends.jl
+++ test/test_backends.jl
@@ -1,7 +1,7 @@
 is_ci() = get(ENV, "CI", "false") == "true"
 
 const TEST_MODULE = Module(:PlotsTestModule)
-const PLOTS_IMG_TOL = parse(Float64, get(ENV, "PLOTS_IMG_TOL", is_ci() ? "1e-2" : "1e-5"))
+const PLOTS_IMG_TOL = parse(Float64, get(ENV, "PLOTS_IMG_TOL", is_ci() ? "2e-3" : "1e-5"))
 
 Base.eval(TEST_MODULE, quote
     using Random, StableRNGs, Plots
