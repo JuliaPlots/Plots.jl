09ea075cda8e5c2061cc76be4bebdd1861c5a190
t-bltg <tf.bltg@gmail.com>
2022-09-27 13:19:25 +0200

    rework precompilation using `SnoopPrecompile` - document `@layout` (#94)
    * document `@layout`
    
    * rewok precompile statements using `SnoopPrecompile`
    
    * add ci
    
    * bump julia to `1.6` for failing CI

--- RecipesBase/src/RecipesBase.jl
+++ RecipesBase/src/RecipesBase.jl
@@ -1,5 +1,6 @@
-
 module RecipesBase
+
+using SnoopPrecompile
 
 export
     @recipe,
@@ -569,9 +570,57 @@ function create_grid(s::Symbol)
     :((label = $(QuoteNode(s)), blank = $(s == :_)))
 end
 
+"""
+    @layout mat
+
+Generate the subplots layout from a matrix of symbols (where subplots can span multiple rows or columns).
+Precise sizing can be achieved with curly brackets, otherwise the free space is equally split between the plot areas of subplots.
+You can use the `_` character (underscore) to ignore plots in the layout (blank plots).
+
+# Examples
+
+```jldoctest
+julia> @layout [a b; c]
+2×1 Matrix{Any}:
+ Any[(label = :a, blank = false) (label = :b, blank = false)]
+ (label = :c, blank = false)
+
+julia> @layout [a{0.3w}; b{0.2h}]
+2×1 Matrix{Any}:
+ (label = :a, width = 0.3, height = :auto)
+ (label = :b, width = :auto, height = 0.2)
+
+julia> @layout [_ ° _; ° ° °; ° ° °]
+3×3 Matrix{Any}:
+ (label = :_, blank = true)   …  (label = :_, blank = true)
+ (label = :°, blank = false)     (label = :°, blank = false)
+ (label = :°, blank = false)     (label = :°, blank = false)
+
+```
+"""
 macro layout(mat::Expr)
     create_grid(mat)
 end
-include("precompile.jl")
 
-end # module
+@precompile_setup begin
+    struct __RecipesBasePrecompileType end
+    @precompile_all_calls begin
+        @layout [a b; c]
+        @layout [a{0.3w}; b{0.2h}]
+        @layout [_ ° _; ° ° °; ° ° °]
+        # @userplot __RecipesBasePrecompileType  # fails (export statements)
+        @recipe f(::__RecipesBasePrecompileType) = begin
+            @series begin
+                markershape --> :auto, :require
+                markercolor --> customcolor, :force
+                xrotation --> 5
+                zrotation --> 6, :quiet
+                fillcolor := :green
+                ones(1)
+            end
+            zeros(1)
+        end
+    end
+end
+
+end
--- RecipesBase/src/precompile.jl
+++
@@ -1,9 +0,0 @@
-function _precompile_()
-    ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
-    Base.precompile(Tuple{typeof(RecipesBase.create_kw_body),Expr})
-    Base.precompile(Tuple{typeof(RecipesBase.get_function_def),Expr,Array{Any,1}})
-    Base.precompile(Tuple{typeof(map),Function,Array{AbstractLayout,1}})
-    Base.precompile(Tuple{typeof(map),Function,Array{AbstractLayout,2}})
-end
-
-_precompile_()
