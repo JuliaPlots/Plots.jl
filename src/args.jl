5de1e67993089266d4dbc7a7a4811ddd1a358e1a
Nicholas Bauer <nicholasbauer@outlook.com>
2022-10-01 14:39:47 -0400

    Fix style

--- src/args.jl
+++ src/args.jl
@@ -1611,7 +1611,7 @@ function warn_on_unsupported_args(pkg::AbstractBackend, plotattributes)
     bend = backend_name(pkg)
     already_warned = get!(_already_warned, bend) do
         Set{Symbol}()
-   end
+    end
     extra_kwargs = Dict{Symbol,Any}()
     for k in explicitkeys(plotattributes)
         is_attr_supported(pkg, k) && !(k in keys(_deprecated_attributes)) && continue
