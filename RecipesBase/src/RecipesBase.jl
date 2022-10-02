6a527f5cd451c8dccd93d308299a61cac8f931f8
t-bltg <tf.bltg@gmail.com>
2022-10-02 11:04:25 +0200

    fix broken docs

--- RecipesBase/src/RecipesBase.jl
+++ RecipesBase/src/RecipesBase.jl
@@ -579,7 +579,7 @@ You can use the `_` character (underscore) to ignore plots in the layout (blank 
 
 # Examples
 
-```jldoctest
+```julia-repl
 julia> @layout [a b; c]
 2×1 Matrix{Any}:
  Any[(label = :a, blank = false) (label = :b, blank = false)]
