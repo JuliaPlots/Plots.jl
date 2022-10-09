91bd30580c9d9da36674c3c59304c0cc8ea863ac
Nicholas Bauer <nicholasbauer@outlook.com>
2022-10-09 13:19:22 -0400

    Format

--- src/precompilation.jl
+++ src/precompilation.jl
@@ -9,28 +9,30 @@ if get(ENV, "PLOTS_PRECOMPILE", "true") == "true"
             _examples[i].external && continue
             (imp = _examples[i].imports) === nothing || push!(imports, imp)
             func = gensym(string(i))
-            push!(examples, quote
-                $func() = begin  # evaluate each example in a local scope
-                    # @show $i  # debug
-                    $(_examples[i].exprs)
-                    if $i == 1  # only for one example
-                        fn = tempname()
-                        pl = current()
-                        gui(pl)
-                        savefig(pl, "$fn.png")
-                        savefig(pl, "$fn.pdf")
+            push!(
+                examples,
+                quote
+                    $func() = begin  # evaluate each example in a local scope
+                        # @show $i  # debug
+                        $(_examples[i].exprs)
+                        if $i == 1  # only for one example
+                            fn = tempname()
+                            pl = current()
+                            gui(pl)
+                            savefig(pl, "$fn.png")
+                            savefig(pl, "$fn.pdf")
+                        end
+                        nothing
                     end
-                    nothing
-                end
-                try
-                    # During precompilation on the Windows GitHub CI in Julia 1.8.2+,
-                    # can fail here due to GR potentially failing to write out the
-                    # temporary file. Pending a fix, swallow the error and continue.
-                    $func()
-                catch
-                    @warn "Plots: Failed a trial save during precompilation"
-                end
-            end)
+                    try
+                        # During precompilation on the Windows GitHub CI in Julia 1.8.2+,
+                        # can fail here due to GR potentially failing to write out the
+                        # temporary file. Pending a fix, swallow the error and continue.
+                        $func()
+                    catch
+                        @warn "Plots: Failed a trial save during precompilation"
+                    end
+                end)
         end
         withenv("GKSwstype" => "nul") do
             @precompile_all_calls begin
