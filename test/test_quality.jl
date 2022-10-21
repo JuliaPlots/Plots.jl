9d221f7753e21bd7e2922e15a09cfecc02dcba97
t-bltg <tf.bltg@gmail.com>
2022-10-21 14:28:16 +0200

    add allocations warning

--- test/test_quality.jl
+++ test/test_quality.jl
@@ -1,3 +1,15 @@
+@testset "allocations" begin
+    if Sys.islinux()
+        with(:gr) do
+            stats = @timed plot(1:2)
+            ref_bytes =  53_196_261  # measured on v1.35.5 - 1.8.2
+            if stats.bytes > ref_bytes
+                @warn "Allocations might have increased ($(stats.bytes) > $ref_bytes)"
+            end
+        end
+    end
+end
+
 @testset "Auto QUality Assurance" begin
     # JuliaTesting/Aqua.jl/issues/77
     # TODO: fix :Contour, :Latexify and :LaTeXStrings stale imports in Plots 2.0
