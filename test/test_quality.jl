df934a2362ef9c488cb38845b01b329c306ef3c6
t-bltg <tf.bltg@gmail.com>
2022-10-22 09:23:08 +0200

    remove non robusttest

--- test/test_quality.jl
+++ test/test_quality.jl
@@ -1,17 +1,3 @@
-@testset "allocations" begin
-    if Sys.islinux()
-        with(:gr) do
-            stats = @timed show(devnull, plot(1:2))
-            @show stats.bytes
-            ref_bytes = 66_406_576  # measured on v1.35.5 - 1.8.2
-            if stats.bytes > ref_bytes
-                @warn "Allocations might have increased ($(stats.bytes) > $ref_bytes)"
-                # only warn, since this might be dependencies or `julia` dependent
-            end
-        end
-    end
-end
-
 @testset "Auto QUality Assurance" begin
     # JuliaTesting/Aqua.jl/issues/77
     # TODO: fix :Contour, :Latexify and :LaTeXStrings stale imports in Plots 2.0
