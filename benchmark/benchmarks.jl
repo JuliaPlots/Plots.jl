6206ac08a366fb30c9ddbf35c060ce3f23cdb2b4
t-bltg <tf.bltg@gmail.com>
2022-11-03 10:59:13 +0100

    simplify benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,7 +4,7 @@ using Plots
 const SUITE = BenchmarkGroup()
 julia_cmd = split(get(ENV, "TESTCMD", unsafe_string(Base.JLOptions().julia_bin)))
 
-SUITE["load_plot_display"] = @benchmarkable run(`$julia_cmd --startup-file=no --project=$(Base.active_project()) -e 'using Plots; display(plot(1:0.1:10, sin.(1:0.1:10)))'`)
+SUITE["load_plot_display"] = @benchmarkable run(`$julia_cmd --startup-file=no --project=$(Base.active_project()) -e 'using Plots; display(plot(1:0.1:10, sin))'`)
 SUITE["load"] = @benchmarkable run(`$julia_cmd --startup-file=no --project=$(Base.active_project()) -e 'using Plots'`)
-SUITE["plot"] = @benchmarkable p = plot(1:0.1:10, sin.(1:0.1:10)) samples=1 evals=1
-SUITE["display"] = @benchmarkable display(p) setup=(p = plot(1:0.1:10, sin.(1:0.1:10))) samples=1 evals=1
+SUITE["plot"] = @benchmarkable p = plot(1:0.1:10, sin) samples=1 evals=1
+SUITE["display"] = @benchmarkable display(p) setup=(p = plot(1:0.1:10, sin)) samples=1 evals=1
