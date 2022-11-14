beec510f1c1f7410a565ef584ccbbe6714efc98e
Simon Christ <SimonChrist@gmx.de>
2022-11-14 14:43:43 +0100

    Update test/test_init.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/test_init.jl
+++ test/test_init.jl
@@ -4,16 +4,16 @@ const plots_path = "\"$(pkgdir(Plots))\""
 
 @testset "Default Backend" begin
     out = withenv("PLOTS_DEFAULT_BACKEND" => "Plotly") do
-         run(```
-            $(Base.julia_cmd()) -E """
-                using Pkg
-                Pkg.activate(; temp = true)
-                Pkg.develop(path = $(plots_path))
-                using Test
-                using Plots
-                @test backend() == Plots.PlotlyBackend()
-                """
-            ```)
+        run(```
+           $(Base.julia_cmd()) -E """
+               using Pkg
+               Pkg.activate(; temp = true)
+               Pkg.develop(path = $(plots_path))
+               using Test
+               using Plots
+               @test backend() == Plots.PlotlyBackend()
+               """
+           ```)
     end
     @test out.exitcode == 0
 end
