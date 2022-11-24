8f9852addf363409f7d11d263bbec7717f9e6671
t-bltg <tf.bltg@gmail.com>
2022-11-24 20:09:51 +0100

    fix unitful bug

--- src/utils.jl
+++ src/utils.jl
@@ -1168,38 +1168,30 @@ _argument_description(s::Symbol) =
 _document_argument(s::Symbol) =
     _fmt_paragraph(_argument_description(s), leadingspaces = 6 + length(string(s)))
 
-#
 # The following functions implement the guess of the optimal legend position,
 # from the data series.
-#
-# Computes the distances of the plot limits to a sample of points at the extremes of 
-# the ranges, and places the legend at the corner where the maximum distance to the limits is found.
-#
 function d_point(x, y, lim, scale)
     p_scaled = (x / scale[1], y / scale[2])
     d = sum(abs2, lim .- p_scaled)
     isnan(d) && return 0.0
-    return d
+    d
 end
-function _dinv_series(lim, scale, x, y, nsamples)
+function _dinv_series(lim, scale, x, y, nsamples, weight = 100.0)
     length(x) > 0 || return +Inf
-    dinv = 0.0
-    step = 1
-    lim = lim ./ scale
-    weight = 100.0
     n = max(1, div(min(nsamples, length(x)), 2))
+    lo, hi = firstindex(x), lastindex(x)
+    lim = lim ./ scale
+    dinv = 0.0
     # Run from the extremes of the dataset inwards
-    for isample in firstindex(x):step:n
+    for isample in lo:n
         dinv += inv(1 + weight * d_point(x[isample], y[isample], lim, scale))
+        jsample = hi - (isample - 1)
+        dinv += inv(1 + weight * d_point(x[jsample], y[jsample], lim, scale))
     end
-    for isample in lastindex(x):(-step):(lastindex(x) - n)
-        dinv += inv(1 + weight * d_point(x[isample], y[isample], lim, scale))
-    end
-    return dinv
+    dinv
 end
-#
+
 # Function barrier because lims are type-unstable
-#
 function _guess_best_legend_position(xl, yl, plt, nsamples)
     scale = (maximum(xl) - minimum(xl), maximum(yl) - minimum(yl))
     dist_to_lims = zeros(4) # faster than tuple
@@ -1212,10 +1204,14 @@ function _guess_best_legend_position(xl, yl, plt, nsamples)
     end
     # this inversion favors :topright in case of draws, without cost
     ibest = findmin(@view(dist_to_lims[4:-1:1]))[2]
-    return (:topright, :topleft, :bottomright, :bottomleft)[ibest]
+    (:topright, :topleft, :bottomright, :bottomleft)[ibest]
 end
-# Main function
-function _guess_best_legend_position(lp::Symbol, plt; nsamples = 50)
+
+"""
+Computes the distances of the plot limits to a sample of points at the extremes of 
+the ranges, and places the legend at the corner where the maximum distance to the limits is found.
+"""
+function _guess_best_legend_position(lp::Symbol, plt, nsamples = 50)
     lp === :best || return lp
-    return _guess_best_legend_position(xlims(plt), ylims(plt), plt, nsamples)
+    _guess_best_legend_position(xlims(plt), ylims(plt), plt, nsamples)
 end
