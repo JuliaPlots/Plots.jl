bd57b8b05d76c4e9780db7c8db8cba4d6de6119f
Simon Christ <SimonChrist@gmx.de>
2022-11-26 14:38:40 +0100

    Update test/test_init.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/test_init.jl
+++ test/test_init.jl
@@ -15,5 +15,9 @@ const plots_path = escape_string(pkgdir(Plots))
                """
            ```)
     @test out.exitcode == 0
-    set_preferences!(Plots, "default_backend" => Plots._fallback_default_backend(), force = true)
+    set_preferences!(
+        Plots,
+        "default_backend" => Plots._fallback_default_backend(),
+        force = true,
+    )
 end
