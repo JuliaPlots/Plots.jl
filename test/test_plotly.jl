767899956fbd4d32dbd6b861aaa87d110a93b294
Simon Christ <christ@cell.uni-hannover.de>
2023-01-27 11:31:17 +0100

    fix test

--- test/test_plotly.jl
+++ test/test_plotly.jl
@@ -4,9 +4,9 @@ with(:plotly) do
         @test backend() == Plots.PlotlyBackend()
 
         pl = plot(rand(10))
-        @test pl isa Plot
+        @test pl isa Plots.Plot
         @test_nowarn Plots.plotly_series(plot())
-        @test !isnan(Plots.plotly_series(pl)[1][:zmax])
+        @test !haskey(Plots.plotly_series(pl)[1], :zmax)
     end
 
     @testset "Contours" begin
