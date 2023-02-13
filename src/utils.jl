65af2641bc18095ae8c1c33f048f75db41ea226e
t-bltg <tf.bltg@gmail.com>
2023-02-13 10:43:26 +0100

    update docstring

--- src/utils.jl
+++ src/utils.jl
@@ -1247,18 +1247,19 @@ macro ext_imp_use(imp_use::QuoteNode, mod::Symbol, args...)
     Expr(imp_use.value, ex) |> esc
 end
 
-# for UnitfulExt - FIXME: this cannot reside in `UnitfulExt` (macro)
+# for UnitfulExt - cannot reside in `UnitfulExt` (macro)
 function protectedstring end
 
 """
     P_str(s)
 
+(Unitful extension only).
 Creates a string that will be Protected from recipe passes.
 
 Example:
 ```julia
+julia> using Unitful
 julia> plot([0,1]u"m", [1,2]u"m/s^2", xlabel=P"This label will NOT display units")
-
 julia> plot([0,1]u"m", [1,2]u"m/s^2", xlabel="This label will display units")
 ```
 """
