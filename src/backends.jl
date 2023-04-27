e4e994475461bea9131d7ef0545519793bdb3f8c
Fons van der Plas <fons@plutojl.org>
2023-04-27 22:03:07 +0200

    improve error log

--- src/backends.jl
+++ src/backends.jl
@@ -583,7 +583,7 @@ function _initialize_backend(pkg::PlotlyBackend)
         _post_imports(pkg)
         _runtime_init(pkg)
     catch err
-        err isa ArgumentError || @warn "Failed to load integration with PlotlyBase & PlotlyKaleide." err
+        err isa ArgumentError || @warn "Failed to load integration with PlotlyBase & PlotlyKaleide." exception=(err,catch_backtrace())
         # NOTE: `plotly` is special in the way that it does not require dependencies for displaying a plot
         # as a result, we cannot rely on the `@require` mechanism for loading glue code
         # this is why it must be done here.
