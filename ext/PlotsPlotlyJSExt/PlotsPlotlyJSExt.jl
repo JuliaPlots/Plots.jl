6aa522072c8fe641081ea7a583999c09b6d4ac46
Simon Christ <simonchrist@gmx.de>
2024-02-02 12:52:42 +0100

    add missing using statement for PlotlyJS

--- ext/PlotsPlotlyJSExt/PlotsPlotlyJSExt.jl
+++ ext/PlotsPlotlyJSExt/PlotsPlotlyJSExt.jl
@@ -1,7 +1,7 @@
 module PlotsPlotlyJSExt
 
 using PlotlyJS: PlotlyJS
-using Plots: Plots, Plot, plotly_show_js
+using Plots: Plots, Plot, plotly_show_js, isplotnull
 import Plots: _show, display, closeall
 
 include("initialization.jl")
