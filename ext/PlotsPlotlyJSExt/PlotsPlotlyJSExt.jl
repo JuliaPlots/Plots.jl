fd5b0422ee90011cedffeafddf28f50c5788c95a
Simon Christ <simonchrist@gmx.de>
2024-02-02 13:27:50 +0100

    more imports

--- ext/PlotsPlotlyJSExt/PlotsPlotlyJSExt.jl
+++ ext/PlotsPlotlyJSExt/PlotsPlotlyJSExt.jl
@@ -1,8 +1,8 @@
 module PlotsPlotlyJSExt
 
 using PlotlyJS: PlotlyJS
-using Plots: Plots, Plot, plotly_show_js, isplotnull
-import Plots: _show, display, closeall
+using Plots: Plots, Plot, plotly_show_js, isplotnull, current
+import Plots: _show, _display, closeall
 
 include("initialization.jl")
 include("plotlyjs.jl")
