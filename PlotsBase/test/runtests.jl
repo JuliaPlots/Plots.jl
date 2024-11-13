0bbbef011acae40480e13025c2bbfc20f814d59b
t-bltg <tf.bltg@gmail.com>
2024-11-13 11:24:54 +0100

    reorder

--- PlotsBase/test/runtests.jl
+++ PlotsBase/test/runtests.jl
@@ -18,7 +18,6 @@ using PlotsBase
 # multiple weakdeps (keep in sync with Project.toml !)
 const WEAKDEPS = Expr(
     :block,
-    :(import Latexify),
     :(import UnitfulLatexify),
     :(import LaTeXStrings),
     :(import Latexify),
