29b98a4a15703db272fc96f876bd2f1e08cf6ad6
Isaac Wheeler <ickaser@gmail.com>
2025-06-17 12:02:23 +0200

    Actually remove ProtectedString

--- PlotsBase/src/utils.jl
+++ PlotsBase/src/utils.jl
@@ -931,43 +931,10 @@ end
 _generate_doclist(attributes) =
     replace(join(sort(collect(attributes)), "\n- "), "_" => "\\_")
 
-"""
-    P_str(s)
-
-(Unitful extension only).
-Creates a string that will not have units appended.
-This is equivalent to passing `[x]unitformat = (l,u) -> l` to the plot command.
-
-Example:
-```julia
-julia> using Unitful
-julia> plot([0,1]u"m", [1,2]u"m/s^2", xlabel=P"This label will NOT display units")
-julia> plot([0,1]u"m", [1,2]u"m/s^2", xlabel="This label will display units")
-```
-"""
-macro P_str(s)
-    protectedstring(s)
-end
 
 # for `PGFPlotsx` together with `UnitfulExt`
 function pgfx_sanitize_string end  # COV_EXCL_LINE
 
-# Old API: a ProtectedString implicitly has unitformat = (l,u) -> l
-# Can be removed for 2.0?
-abstract type AbstractProtectedString <: AbstractString end
-struct ProtectedString{S} <: AbstractProtectedString
-    content::S
-end
-const APS = AbstractProtectedString
-# Minimum required AbstractString interface to work with PlotsBase
-Base.iterate(n::APS) = iterate(n.content)
-Base.iterate(n::APS, i::Integer) = iterate(n.content, i)
-Base.codeunit(n::APS) = codeunit(n.content)
-Base.ncodeunits(n::APS) = ncodeunits(n.content)
-Base.isvalid(n::APS, i::Integer) = isvalid(n.content, i)
-Base.pointer(n::APS) = pointer(n.content)
-Base.pointer(n::APS, i::Integer) = pointer(n.content, i)
-protectedstring(s) = ProtectedString(s)
 
 function extrema_plus_buffer(v, buffmult = 0.2)
     vmin, vmax = ignorenan_extrema(v)
