c7090b3550aa2dd580cd4a3abbc6fe41b4cba578
t-bltg <tf.bltg@gmail.com>
2025-07-03 16:03:58 +0200

    update

--- PlotsBase/src/plotly.jl
+++ PlotsBase/src/plotly.jl
@@ -1280,26 +1280,25 @@ function plotly_html_body(plt, style = nothing)
         requirejs_suffix = "});"
     end
 
-    uuid = replace(string(UUIDs.uuid4()), '-' => '_')
+    unique_tag = replace(string(UUIDs.uuid4()), '-' => '_')
 
-    html = """
-        <div id=\"$(uuid)\" style=\"$(style)\"></div>
-        <script>
-        function plots_jl_plotly_$(uuid)() {
-            $(requirejs_prefix)
-            $(js_body(plt, uuid))
-            $(requirejs_suffix)
-        }
-        </script>
-        <script src="https://requirejs.org/docs/release/$(PlotsBase._requirejs_version)/minified/require.js" onload="plots_jl_plotly_$(uuid)()"></script>
+    return """
+    <div id=\"$unique_tag\" style=\"$style\"></div>
+    <script>
+    function plots_jl_plotly_$unique_tag() {
+        $requirejs_prefix
+        $(js_body(plt, unique_tag))
+        $requirejs_suffix
+    }
+    </script>
+    <script src="https://requirejs.org/docs/release/$(PlotsBase._requirejs_version)/minified/require.js" onload="plots_jl_plotly_$unique_tag()"></script>
     """
-    return html
 end
 
 js_body(
     plt::Plot,
-    uuid,
-) = "Plotly.newPlot('$(uuid)', $(plotly_series_json(plt)), $(plotly_layout_json(plt)));"
+    unique_tag,
+) = "Plotly.newPlot('$unique_tag', $(plotly_series_json(plt)), $(plotly_layout_json(plt)));"
 
 plotly_show_js(io::IO, plot::Plot) =
     JSON.print(io, Dict(:data => plotly_series(plot), :layout => plotly_layout(plot)))
