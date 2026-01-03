883112bc5eca4e07cf8fc4f18b97bd726e02d848
t-bltg <tf.bltg@gmail.com>
2026-01-03 22:27:04 +0100

    restructure repository (#5211)

---
+++ Plots/test/runtests.jl
@@ -0,0 +1,30 @@
+using Pkg
+Pkg.status(; outdated = true, mode = Pkg.PKGMODE_MANIFEST)
+
+const TEST_PACKAGES = let val = get(ENV, "PLOTS_TEST_PACKAGES", "GR,UnicodePlots,PythonPlot")
+    Symbol.(strip.(split(val, ",")))
+end
+const TEST_BACKENDS = NamedTuple(p => Symbol(lowercase(string(p))) for p in TEST_PACKAGES)
+
+using Plots
+const PlotsBase = Plots.PlotsBase
+
+# initialize all backends
+for pkg in TEST_PACKAGES
+    @eval begin
+        import $pkg  # trigger extension
+        $(TEST_BACKENDS[pkg])()
+    end
+end
+gr()
+
+using Test
+
+for pkg in TEST_PACKAGES
+    @testset "simple plots using $pkg" begin
+        @eval $(TEST_BACKENDS[pkg])()
+        pl = plot(1:2)
+        @test pl isa PlotsBase.Plot
+        show(devnull, pl)
+    end
+end
