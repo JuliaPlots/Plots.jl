6097a36850a248e3d79dc5e5bd85b3df2e9f2de6
t-bltg <tf.bltg@gmail.com>
2026-01-09 20:33:09 +0100

    fix `ispositive` on `pre` (#5217)

--- PlotsBase/src/plot.jl
+++ PlotsBase/src/plot.jl
@@ -308,3 +308,53 @@ function plot!(sp::Subplot, args...; kw...)
     plt = sp.plt
     return plot!(plt, args...; kw..., subplot = findfirst(isequal(sp), plt.subplots))
 end
+
+"account for the size/length/rotation of tick labels"
+function tick_padding(sp::Subplot, axis::Axis)
+    return if (ticks = get_ticks(sp, axis)) ≡ nothing
+        0mm
+    else
+        vals, labs = ticks
+        isempty(labs) && return 0mm
+        # ptsz = axis[:tickfont].pointsize * pt
+        longest_label = maximum(length(lab) for lab in labs)
+
+        # generalize by "rotating" y labels
+        rot = axis[:rotation] + (axis[:letter] ≡ :y ? 90 : 0)
+
+        #=
+        # we need to compute the size of the ticks generically
+        # this means computing the bounding box and then getting the width/height
+        labelwidth = 0.8longest_label * ptsz
+
+        # now compute the generalized "height" after rotation as the "opposite+adjacent" of 2 triangles
+        hgt = abs(sind(rot)) * labelwidth + abs(cosd(rot)) * ptsz + 1mm
+        =#
+
+        # get the height of the rotated label
+        text_size(longest_label, axis[:tickfontsize], rot)[2]
+    end
+end
+
+"""
+Set the (left, top, right, bottom) minimum padding around the plot area
+to fit ticks, tick labels, guides, colorbars, etc.
+"""
+function _update_min_padding!(sp::Subplot)
+    # TODO: something different when `RecipesPipeline.is3d(sp) == true`
+    leftpad = tick_padding(sp, sp[:yaxis]) + sp[:left_margin] + guide_padding(sp[:yaxis])
+    toppad = sp[:top_margin] + title_padding(sp)
+    rightpad = sp[:right_margin]
+    bottompad = tick_padding(sp, sp[:xaxis]) + sp[:bottom_margin] + guide_padding(sp[:xaxis])
+
+    # switch them?
+    if sp[:xaxis][:mirror]
+        bottompad, toppad = toppad, bottompad
+    end
+    if sp[:yaxis][:mirror]
+        leftpad, rightpad = rightpad, leftpad
+    end
+
+    # @show (leftpad, toppad, rightpad, bottompad)
+    return sp.minpad = (leftpad, toppad, rightpad, bottompad)
+end
