72f72d4f9b5f8a0acce0d190f87d73628eda44fd
partyplatter08-lab <226721044+partyplatter08-lab@users.noreply.github.com>
2026-06-04 10:34:38 -0400

    Fix GR colorbar axis argument type

--- PlotsBase/ext/GRExt.jl
+++ PlotsBase/ext/GRExt.jl
@@ -883,7 +883,7 @@ function gr_draw_colorbar(cbar::GRColorbar, sp::Subplot, vp::GRViewport)
                 gr_set_font(colorbartickfont(sp), sp)
                 axis = if is_horizontal
                     GR.axis(
-                        'X';
+                        "X";
                         min = x_min,
                         max = x_max,
                         org = x_min,
@@ -895,7 +895,7 @@ function gr_draw_colorbar(cbar::GRColorbar, sp::Subplot, vp::GRViewport)
                     )
                 else
                     GR.axis(
-                        'Y';
+                        "Y";
                         min = z_min,
                         max = z_max,
                         org = z_min,
