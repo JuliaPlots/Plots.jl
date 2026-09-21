cc98f640bca7dc6daf386c9946dbabd19c7c1b06
Jash Ambaliya <jashambaliya1@gmail.com>
2026-09-21 14:47:39 +0530

    Say argument rather than datum in the push!/append! comments

--- PlotsBase/src/Plots.jl
+++ PlotsBase/src/Plots.jl
@@ -82,12 +82,12 @@ Base.iterate(plt::Plot) = iterate(plt.subplots)
 # -------------------------------------------------------
 # push/append for one series
 
-# the indexed methods take at least one datum, else `push!(plt, 4)` would read the `4` as a
+# the indexed methods take at least one argument, else `push!(plt, 4)` would read the `4` as a
 # series index and push nothing
 Base.push!(plt::Plot, args::Real...) = push!(plt, 1, args...)
 Base.push!(plt::Plot, i::Integer, arg::Real, args::Real...) =
     push!(plt.series_list[i], arg, args...)
-# the un-indexed `append!` also takes at least one datum, else the `Real` and the
+# the un-indexed `append!` also takes at least one argument, else the `Real` and the
 # `AbstractVector` method would both match `append!(plt)` and be ambiguous
 Base.append!(plt::Plot, arg::Real, args::Real...) = append!(plt, 1, arg, args...)
 Base.append!(plt::Plot, i::Integer, arg::Real, args::Real...) =
